8f84203f6e8f7819328aac773944fa3799a879cf
vietman2 <vietman0@gmail.com>
2024-08-06 20:36:11 +0900

    start user app

---
+++ server/user/models.py
@@ -0,0 +1,28 @@
+import uuid
+from django.contrib.auth.models import AbstractBaseUser
+from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
+
+class User(AbstractBaseUser):
+    uuid        = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    username    = models.CharField(max_length=150, unique=True)
+    email       = models.EmailField(max_length=255, unique=True)
+    phone       = PhoneNumberField(unique=True)
+
+    first_name  = models.CharField(max_length=150, blank=True)
+    last_name   = models.CharField(max_length=150, blank=True)
+
+    is_active   = models.BooleanField(default=True)
+
+    joined_at   = models.DateTimeField(auto_now_add=True)
+    deleted_at  = models.DateTimeField(null=True, blank=True)
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['email', 'phone']
+
+    @property
+    def full_name(self):
+        return f'{self.first_name} {self.last_name}'
+
+    class Meta:
+        db_table = 'user'
