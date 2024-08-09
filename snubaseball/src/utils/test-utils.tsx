86804b77bf0cd091c7ca193d5385078794f5e799
vietman2 <vietman0@gmail.com>
2024-08-09 22:58:41 +0900

    add test utils

---
+++ snubaseball/src/utils/test-utils.tsx
@@ -0,0 +1,25 @@
+import { PropsWithChildren, ReactElement } from "react";
+import { Provider } from "react-redux";
+import { render, RenderOptions } from "@testing-library/react";
+
+import { RootState, AppStore, setupStore } from "@store/index";
+
+interface ExtendedRenderOptions extends Omit<RenderOptions, "queries"> {
+  preloadedState?: Partial<RootState>;
+  store?: AppStore;
+}
+
+export function renderWithProviders(
+  ui: ReactElement,
+  {
+    preloadedState = {},
+    store = setupStore(preloadedState),
+    ...renderOptions
+  }: ExtendedRenderOptions = {}
+) {
+  function Wrapper({ children }: PropsWithChildren): JSX.Element {
+    return <Provider store={store}>{children}</Provider>;
+  }
+
+  return { store, ...render(ui, { wrapper: Wrapper, ...renderOptions }) };
+}
