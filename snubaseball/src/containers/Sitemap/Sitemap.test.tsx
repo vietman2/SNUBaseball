ad2efb5b8e81b67d5c15c000d920a1b5ef7c30cf
vietman2 <vietman0@gmail.com>
2024-08-09 23:04:05 +0900

    add simple test files

---
+++ snubaseball/src/containers/Sitemap/Sitemap.test.tsx
@@ -0,0 +1,9 @@
+import { render } from "@testing-library/react";
+
+import Sitemap from "./Sitemap";
+
+describe("<Sitemap />", () => {
+  it("should render", () => {
+    render(<Sitemap />);
+  });
+});
