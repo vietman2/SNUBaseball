53ad11c87f26c163ed67cac88029b91251a533f4
vietman2 <vietman0@gmail.com>
2024-08-09 23:00:10 +0900

    refactor and add pages

---
+++ snubaseball/src/containers/root/App.test.tsx
@@ -0,0 +1,19 @@
+import { render } from "@testing-library/react";
+
+import App from "./App";
+
+jest.mock("./RootLayout/RootLayout", () => () => <div>Mock RootLayout</div>);
+jest.mock("containers/Home/Home", () => "Home");
+jest.mock("containers/Archive/Archive", () => "Archive");
+jest.mock("containers/Sitemap/Sitemap", () => "Sitemap");
+jest.mock("containers/Ask/Ask", () => "Ask");
+jest.mock("containers/About/About", () => "About");
+jest.mock("containers/Schedule/Schedule", () => "Schedule");
+jest.mock("containers/Login/Login", () => "Login");
+jest.mock("containers/Signup/Signup", () => "Signup");
+
+describe("<App />", () => {
+  it("should render without crashing", () => {
+    render(<App />);
+  });
+});
