f6122039db72f0f25379a863ad2c801cd6aba06e
vietman2 <vietman0@gmail.com>
2024-08-14 13:48:43 +0900

    refactor components

--- snubaseball/src/containers/Archive/Archive.tsx
+++ snubaseball/src/containers/Archive/Archive.tsx
@@ -1,117 +1,42 @@
-import { useEffect, useState } from "react";
 import styled from "styled-components";
 
-import { Divider } from "@components/Dividers";
-import { getImages } from "services/archive/images";
-import { Interview, Memories } from "fragments/Archive";
-
-type ImageType = {
-  id: number;
-  title: string;
-  file: string;
-};
+import { Divider, TextDivider } from "@components/Dividers";
+import { EmptyTabs } from "@components/Tabs";
+import { Title } from "@components/Texts";
+import { Interview, Memories } from "@fragments/Archive";
 
 export default function Archive() {
-  const [images, setImages] = useState<ImageType[]>([]);
-
-  const fetchImages = async () => {
-    try {
-      const response = await getImages();
-
-      if (response.status === 200) {
-        setImages(response.data);
-      }
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
   const navigateMore = () => {};
 
-  useEffect(() => {
-    fetchImages();
-  }, []);
-
   return (
-    <Container>
-      <Title>
-        MEMORIES <Subtitle>_ 순간의 기록</Subtitle>
-      </Title>
-      <Line>
-        <YearText>2024</YearText>
-      </Line>
-      <ImageContainer>{images ? <Memories /> : null}</ImageContainer>
-      <Line>
-        <YearText>2023</YearText>
-      </Line>
-      <ImageContainer>{images ? <Memories /> : null}</ImageContainer>
-      <Divider />
-      <Button onClick={navigateMore}>{"MORE >>"}</Button>
-      <Title>
-        INTERVIEW <Subtitle>_ 우리들의 이야기</Subtitle>
-      </Title>
-      <Line />
-      <Interview />
-      <Divider />
-      <Button onClick={navigateMore}>{"MORE >>"}</Button>
-    </Container>
+    <>
+      <EmptyTabs />
+      <Container>
+        <Title title="MEMORIES" subtitle="순간의 기록" />
+        <TextDivider text="2024" />
+        <ImageContainer>
+          <Memories />
+        </ImageContainer>
+        <TextDivider text="2023" />
+        <ImageContainer>
+          <Memories />
+        </ImageContainer>
+        <Divider />
+        <Button onClick={navigateMore}>{"MORE >>"}</Button>
+        <Title title="INTERVIEW" subtitle="우리들의 이야기" />
+        <Interview />
+        <Divider />
+        <Button onClick={navigateMore}>{"MORE >>"}</Button>
+      </Container>
+    </>
   );
 }
 
 const Container = styled.div`
   display: flex;
-  flex: 1;
   flex-direction: column;
   margin-top: 10px;
-  padding: 0 10px;
-`;
-
-const Title = styled.div`
-  display: flex;
-  align-items: flex-end;
-  font-size: 20px;
-  font-weight: bold;
-  color: black;
-  margin-top: 10px;
-  padding: 5px 20px;
-`;
-
-const Subtitle = styled.div`
-  font-size: 14px;
-  color: black;
-`;
-
-const Line = styled.div`
-  position: relative;
-  height: 1px;
-  margin: 15px 0 5px 0;
-  background: repeating-linear-gradient(
-    to right,
-    black 0,
-    black 2px,
-    transparent 2px,
-    transparent 6px,
-    black 6px,
-    black 8px,
-    transparent 8px,
-    transparent 12px,
-    black 12px,
-    black 20px,
-    transparent 20px,
-    transparent 24px
-  );
-`;
-
-const YearText = styled.span`
-  position: absolute;
-  right: 0;
-  top: -15px;
-  background-color: white;
-  padding-left: 15px;
-  padding-right: 20px;
-  font-size: 20px;
-  color: #0f0f70;
-  z-index: 1;
+  padding: 0px 10px 40px 10px;
 `;
 
 const ImageContainer = styled.div`
