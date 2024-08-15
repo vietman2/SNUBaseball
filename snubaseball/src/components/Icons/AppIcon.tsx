30cbf402608d0a8e904060dbd518d15d6fcb5cc1
vietman2 <vietman0@gmail.com>
2024-08-15 22:59:22 +0900

    add more icons

--- snubaseball/src/components/Icons/AppIcon.tsx
+++ snubaseball/src/components/Icons/AppIcon.tsx
@@ -1,3 +1,6 @@
+import { ReactComponent as BaseballIcon } from "./baseball.svg";
+import { ReactComponent as BatIcon } from "./bat.svg";
+import { ReactComponent as CheckboxIcon } from "./checkbox.svg";
 import { ReactComponent as MenuIcon } from "./menu.svg";
 import { ReactComponent as UserIcon } from "./user.svg";
 
@@ -13,6 +16,12 @@ export const AppIcon = ({ icon, size, color }: Props) => {
       return <MenuIcon width={size} height={size} color={color} />;
     case "user":
       return <UserIcon width={size} height={size} color={color} />;
+    case "baseball":
+      return <BaseballIcon width={size} height={size} color={color} />;
+    case "bat":
+      return <BatIcon width={size} height={size} color={color} />;
+    case "checkbox":
+      return <CheckboxIcon width={size} height={size} color={color} />;
     default:
       return null;
   }
