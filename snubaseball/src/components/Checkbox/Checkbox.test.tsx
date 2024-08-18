2fac7708d01df6d2e95cba7d598aafe1e6134c95
vietman2 <vietman0@gmail.com>
2024-08-18 18:40:15 +0900

    minor test config fix

--- snubaseball/src/components/Checkbox/Checkbox.test.tsx
+++ snubaseball/src/components/Checkbox/Checkbox.test.tsx
@@ -2,6 +2,8 @@ import { render } from "@testing-library/react";
 
 import { Checkbox } from "./Checkbox";
 
+jest.unmock("@components/Checkbox");
+
 describe("<Checkbox />", () => {
   it("should render", () => {
     render(<Checkbox text="asdf" />);
