ffc278f208b38ccc393513b362f4ebeadf7182d9
vietman2 <vietman0@gmail.com>
2024-09-08 15:30:19 +0900

    test config

--- baseballhub/src/setupTests.tsx
+++ baseballhub/src/setupTests.tsx
@@ -17,15 +17,23 @@ jest.mock("react-router-dom", () => {
   };
 });
 
+jest.mock("@assets/images/logo.png", () => "logo.png");
 jest.mock("@components/Buttons", () => ({
   TextButton: ({ text, onClick }: { text: string; onClick: () => void }) => (
-    <button onClick={onClick}>{text}</button>
+    <button onClick={onClick} data-testid={`button-${text}`}>
+      {text}
+    </button>
   ),
 }));
 jest.mock("@components/Chips", () => ({
   Chip: ({ label, onClick }: { label: string; onClick: () => void }) => (
-    <button onClick={onClick}>{label}</button>
+    <button onClick={onClick} data-testid={label}>
+      {label}
+    </button>
   ),
+}));
+jest.mock("@components/Frames", () => ({
+  IFrame: () => <div>IFrame</div>,
 }));
 jest.mock("@components/Icons", () => ({
   AppIcon: ({ icon }: { icon: string }) => <span>{icon}</span>,
@@ -46,6 +54,12 @@ jest.mock("@components/Inputs", () => ({
     onChange: (value: string) => void;
   }) => <input value={value} onChange={(e) => onChange(e.target.value)} />,
 }));
+jest.mock("@components/Menus", () => ({
+  MenuList: () => <div>MenuList</div>,
+}));
+jest.mock("@components/Modals", () => ({
+  NewMemberModal: () => <div>NewMemberModal</div>,
+}));
 jest.mock("@components/RootLayout", () => ({
   RootLayout: ({ children }: { children: React.ReactNode }) => (
     <div>{children}</div>
@@ -56,8 +70,26 @@ jest.mock("@components/Selectors", () => ({
   CollegeSelector: () => <div>CollegeSelector</div>,
 }));
 jest.mock("@components/Sidebar", () => ({
-  Sidebar: ({toggleSidebar}: {toggleSidebar: () => void}) => <div onClick={toggleSidebar} data-testid="sidebar">Sidebar</div>,
+  Sidebar: ({ toggleSidebar }: { toggleSidebar: () => void }) => (
+    <div onClick={toggleSidebar} data-testid="sidebar">
+      Sidebar
+    </div>
+  ),
+}));
+jest.mock("@components/Tables", () => ({
+  DailyTable: () => <div>DailyTable</div>,
+  MembersTable: () => <div>MembersTable</div>,
+  WeeklyTable: () => <div>WeeklyTable</div>,
 }));
 jest.mock("@components/Tabs", () => ({
   Tabs: () => <div>Tabs</div>,
+}));
+jest.mock("@pages/Auth/AuthProvider", () => ({
+  AuthProvider: ({ children }: { children: React.ReactNode }) => (
+    <>{children}</>
+  ),
+  useAuth: () => ({
+    logout: jest.fn(),
+    login: jest.fn(),
+  }),
 }));
