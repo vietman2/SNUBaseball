8d8cd422078dcbc284e40f9a429b447f3d13dd2e
vietman2 <vietman0@gmail.com>
2024-09-15 22:33:22 +0900

    reconfigure tabs

--- baseballhub/src/constants/navigation/tabs.ts
+++ baseballhub/src/constants/navigation/tabs.ts
@@ -1,79 +1,44 @@
-type SubTab = {
+type Tab = {
   title: string;
+  icon: string;
   path: string;
 };
 
-export type Tab = {
+export type TabGroup = {
   title: string;
-  icon: string;
-  path?: string;
-  submenu?: SubTab[];
+  tabs: Tab[];
+  limited: boolean;
 };
 
-export const sidebarItems: Tab[] = [
-  { title: "Home", icon: "home", path: "/home" },
-  {
-    title: "스케줄",
-    icon: "calendar",
-    submenu: [
-      { title: "훈련참가표", path: "/schedule/weekly" },
-      {
-        title: "일자별 훈련계획",
-        path: "/schedule/daily",
-      },
-      {
-        title: "월별 일정",
-        path: "/",
-      },
-      {
-        title: "출석관리",
-        path: "/",
-      },
-    ],
-  },
-  {
-    title: "훈련",
-    icon: "field",
-    submenu: [
-      { title: "훈련 가이드라인", path: "/training/guidelines" },
-      { title: "훈련일지", path: "/training/journals" },
-      { title: "피드백", path: "/training/feedback" },
-    ],
-  },
-  {
-    title: "자료실",
-    icon: "archive",
-    submenu: [{ title: "Gallery", path: "/gallery" }],
-  },
-  {
-    title: "기록실",
-    icon: "record",
-    submenu: [{ title: "경기결과", path: "/records/results" }],
-  },
-  {
-    title: "게시판",
-    icon: "forum",
-    submenu: [
-      { title: "자유게시판", path: "/forum/board" },
-      { title: "팀 관리", path: "/forum/management" },
-    ],
-  },
-  {
-    title: "관리자 메뉴",
-    icon: "admin",
-    submenu: [
-      {
-        title: "부원 관리",
-        path: "/admin/members",
-      },
-      {
-        title: "훈련계획 관리",
-        path: "/",
-      },
-      {
-        title: "아카이브 관리",
-        path: "/",
-      },
-    ],
-  },
-];
+const Main: TabGroup = {
+  title: "Main",
+  tabs: [
+    { title: "Home", icon: "home", path: "/home" },
+    { title: "기록실", icon: "record", path: "/records" },
+    { title: "게시판", icon: "forum", path: "/forum" },
+    { title: "갤러리", icon: "gallery", path: "/gallery" },
+    { title: "자료실", icon: "archive", path: "/archive" },
+  ],
+  limited: false,
+};
+
+const Training: TabGroup = {
+  title: "Training",
+  tabs: [
+    { title: "훈련 일정", icon: "calendar", path: "/guidelines" },
+    { title: "훈련 가이드라인", icon: "guide", path: "/guidelines" },
+    { title: "훈련 일지", icon: "diary", path: "/journals" },
+    { title: "피드백", icon: "feedback", path: "/feedback" },
+  ],
+  limited: false,
+};
+
+const Admin: TabGroup = {
+  title: "Admin",
+  tabs: [
+    { title: "부원 관리", icon: "person", path: "/members" },
+  ],
+  limited: true,
+};
+
+export const tabgroups: TabGroup[] = [Main, Training, Admin];
