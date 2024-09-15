3cbb790594a523ec20fd925bc78fe7f02be8da81
vietman2 <vietman0@gmail.com>
2024-09-15 22:33:35 +0900

    add colors

--- baseballhub/src/constants/colors/palette.ts
+++ baseballhub/src/constants/colors/palette.ts
@@ -1,10 +1,11 @@
 export const palette = {
   primary: "#0f0f70",
-  secondary: "#ffffff",
+  secondary: "#003366",
   background: "#f5f5f5",
   highEmphasis: "#1a1a8f",
   lowEmphasis: "#333333",
   grayBorder: "#cccccc",
   coldSteel: "#e3edf8",
-  charcoal: "#252525"
+  charcoal: "#252525",
+  mystic: "#b1bdcd",
 };
