e0382d751ce656acffd6def9c277464703e82f5f
vietman2 <vietman0@gmail.com>
2024-09-15 23:35:27 +0900

    config fix

--- baseballhub/eslint.config.js
+++ baseballhub/eslint.config.js
@@ -17,10 +17,6 @@ module.exports = [
     rules: {
       semi: "error",
     },
-    extends: [
-      "@babel/plugin-transform-private-property-in-object",
-      "next/core-web-vitals",
-    ],
   },
   ...tseslint.configs.recommended,
 ];
