e1bf866da8cb2b1cb6e9f9cf85cd9b9a7a54bc8a
vietman2 <vietman0@gmail.com>
2024-09-15 10:21:44 +0900

    lint fixes

--- baseballhub/src/constants/data/forum.ts
+++ baseballhub/src/constants/data/forum.ts
@@ -1,5 +1,15 @@
 import { PostType, CommentType } from "@models/forum";
 
+export const sampleComments: CommentType[] = [
+  {
+    id: 1,
+    content: "유감입니다.",
+    author: "김유안",
+    created_at: "2024-09-08",
+    updated_at: "2024-09-08",
+  },
+];
+
 export const samplePosts: PostType[] = [
   {
     id: 1,
@@ -9,14 +19,6 @@ export const samplePosts: PostType[] = [
     created_at: "2024-09-08",
     updated_at: "2024-09-08",
     tag: "뻘소리",
-    comments: [
-      {
-        id: 1,
-        content: "유감입니다.",
-        author: "김유안",
-        created_at: "2024-09-08",
-        updated_at: "2024-09-08",
-      },
-    ],
+    comments: sampleComments,
   },
 ];
--- baseballhub/src/pages/Training/Feedback/Feedback.tsx
+++ baseballhub/src/pages/Training/Feedback/Feedback.tsx
@@ -1,38 +1,19 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import styled from "styled-components";
 
-const samplePlayers = [
-  "전체",
-  "김유안",
-  "양서진",
-  "이상현",
-  "유호성",
-  "허준서",
-  "김택원",
-  "심민수",
-  "이두희",
-];
-
-const tabs = ["내야 수비", "외야 수비", "타격", "스로잉", "주루"];
+//const tabs = ["내야 수비", "외야 수비", "타격", "스로잉", "주루"];
 
 export default function Feedback() {
   const [semester, setSemester] = useState<string>("");
-  const [players, setPlayers] = useState<string[]>([]);
-  const [selectedPlayer, setSelectedPlayer] = useState<string>("");
-  const [activeTab, setActiveTab] = useState<string>("내야 수비");
-
-  useEffect(() => {
-    setPlayers(samplePlayers);
-    setSemester("2024-2학기");
-    setSelectedPlayer("전체");
-  }, []);
+  //const [players, setPlayers] = useState<string[]>([]);
+  //const [selectedPlayer, setSelectedPlayer] = useState<string>("");
+  //const [activeTab, setActiveTab] = useState<string>("내야 수비");
 
   return (
-      <Container>
-        <Title>{semester} Feedback</Title>
-        <Contents>
-        </Contents>
-      </Container>
+    <Container>
+      <Title>{semester} Feedback</Title>
+      <Contents></Contents>
+    </Container>
   );
 }
 
