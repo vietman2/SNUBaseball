e74b90c6b32ceeef307e5b79ae67b4fac55c757f
vietman2 <vietman0@gmail.com>
2024-09-15 11:07:43 +0900

    test

--- server/archive/memories/tests.py
+++ server/archive/memories/tests.py
@@ -1,3 +1,12 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+class MemoriesAPITestCase(APITestCase):
+    fixtures = ['data/test/memories.json']
+
+    def setUp(self):
+        self.url = '/api/memories/'
+
+    def test_get_memories(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.data), 4)
