135bc86a8e55c993148e6f848fc9a121ca8c8582
vietman2 <vietman0@gmail.com>
2024-09-15 10:15:52 +0900

    test setup

--- baseballhub/src/setupTests.tsx
+++ baseballhub/src/setupTests.tsx
@@ -18,6 +18,7 @@ jest.mock("react-router-dom", () => {
 });
 
 jest.mock("@assets/images/logo.png", () => "logo.png");
+
 jest.mock("@components/Buttons", () => ({
   TextButton: ({ text, onClick }: { text: string; onClick: () => void }) => (
     <button onClick={onClick} data-testid={`button-${text}`}>
@@ -58,7 +59,11 @@ jest.mock("@components/Menus", () => ({
   MenuList: () => <div>MenuList</div>,
 }));
 jest.mock("@components/Modals", () => ({
-  NewMemberModal: () => <div>NewMemberModal</div>,
+  NewMemberModal: ({ onClose }: { onClose: () => void }) => (
+    <div onClick={onClose} data-testid="close">
+      NewMemberModal
+    </div>
+  ),
 }));
 jest.mock("@components/RootLayout", () => ({
   RootLayout: ({ children }: { children: React.ReactNode }) => (
