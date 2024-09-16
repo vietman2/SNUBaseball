24055fc67073ab6c03b8e8b127787ca0dff13270
vietman2 <vietman0@gmail.com>
2024-09-16 21:18:42 +0900

    apply color changes

--- baseballhub/src/fragments/Post/PostSimple/PostSimple.tsx
+++ baseballhub/src/fragments/Post/PostSimple/PostSimple.tsx
@@ -31,7 +31,7 @@ const Container = styled.div`
   padding: 0.5rem 1rem;
   margin-bottom: 1rem;
   border-radius: 15px;
-  background-color: ${palette.background};
+  background-color: ${palette.fullWhite};
 
   div {
     display: flex;
