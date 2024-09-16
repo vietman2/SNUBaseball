e92b474be5958e85dc925060199adae1d6be4a20
vietman2 <vietman0@gmail.com>
2024-09-16 23:21:07 +0900

    update test

--- baseballhub/src/pages/Records/Results/GameDetail.test.tsx
+++ baseballhub/src/pages/Records/Results/GameDetail.test.tsx
@@ -2,6 +2,11 @@ import { render } from "@testing-library/react";
 
 import { GameDetail } from "./GameDetail";
 
+jest.mock("@fragments/Records", () => ({
+  GameStory: () => <div>GameStory</div>,
+  GameReport: () => <div>GameReport</div>,
+}));
+
 describe("<GameDetail />", () => {
   it("renders", () => {
     render(<GameDetail selectedGame={1} goBack={jest.fn()} />);
