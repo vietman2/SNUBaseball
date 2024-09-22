4e31664ad4a0a704d4199033e9cb59e4c95fad0b
vietman2 <vietman0@gmail.com>
2024-09-22 16:48:19 +0900

    remove unused colors

--- baseballhub/src/constants/themes/themeColors.ts
+++ baseballhub/src/constants/themes/themeColors.ts
@@ -17,14 +17,4 @@ export const light = {
   // 배경
   background: "#B1BDCD",
   // B5B6B6, E8E6F2, E0DFF4, B1BDCD
-
-  //activeTab: "#b1bdcd",
-  //contentBackground: "#eef4f4",
-
-  //winBackground: "rgba(0, 255, 64, 0.75)",
-  //drawBackground: "rgba(128, 128, 128, 0.2)",
-  //loseBackground: "rgba(255, 64, 64, 0.5)",
-
-  //highEmphasis: "#1a1a8f",
-  //lowEmphasis: "#333333",
 };
