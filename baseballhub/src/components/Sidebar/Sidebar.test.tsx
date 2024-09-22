85dfc5e1a00c2063d81b54e41b264c7d1dec8934
vietman2 <vietman0@gmail.com>
2024-09-22 16:36:59 +0900

    fix tests

--- baseballhub/src/components/Sidebar/Sidebar.test.tsx
+++ baseballhub/src/components/Sidebar/Sidebar.test.tsx
@@ -1,19 +1,24 @@
-import { fireEvent, render, screen } from "@testing-library/react";
+import { fireEvent, screen } from "@testing-library/react";
 
 import { Sidebar } from "./Sidebar";
+import { renderWithProviders } from "@utils/test-utils";
 
 jest.unmock("@components/Sidebar");
 
 describe("<Sidebar />", () => {
   it("should render: open", async () => {
-    render(<Sidebar isSidebarOpen={true} toggleSidebar={jest.fn()} />);
+    renderWithProviders(
+      <Sidebar isSidebarOpen={true} toggleSidebar={jest.fn()} />
+    );
 
     fireEvent.mouseEnter(screen.getByTestId("Home"));
     fireEvent.click(screen.getByTestId("toggle"));
   });
 
   it("should render: closed", () => {
-    render(<Sidebar isSidebarOpen={false} toggleSidebar={jest.fn()} />);
+    renderWithProviders(
+      <Sidebar isSidebarOpen={false} toggleSidebar={jest.fn()} />
+    );
 
     fireEvent.click(screen.getByTestId("Home"));
   });
