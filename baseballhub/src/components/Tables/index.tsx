e85510806761b117e962bdcb1b6739d850b23878
vietman2 <vietman0@gmail.com>
2024-09-30 18:47:28 +0900

    remove unused table

--- baseballhub/src/components/Tables/index.tsx
+++ baseballhub/src/components/Tables/index.tsx
@@ -1,5 +1,4 @@
 import { DailyTable } from "./DailyTable";
 import { MembersTable } from "./MembersTable";
-import { WeeklyTable } from "./WeeklyTable";
 
-export { DailyTable, MembersTable, WeeklyTable };
+export { DailyTable, MembersTable };
