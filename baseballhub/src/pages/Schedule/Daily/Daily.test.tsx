c36edb7ba5ac66c6943c2be9b056c76e6c1b6407
vietman2 <vietman0@gmail.com>
2024-09-30 21:46:33 +0900

    create and work schedule container

--- baseballhub/src/pages/Schedule/Daily/Daily.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render } from "@testing-library/react";
-
-import Daily from "./Daily";
-
-describe("<Daily />", () => {
-  it("should render without errors", () => {
-    render(<Daily />);
-  });
-});
---
+++ baseballhub/src/pages/Schedule/ScheduleContainer.test.tsx
@@ -0,0 +1,18 @@
+import { fireEvent, screen } from "@testing-library/react";
+import ScheduleContainer from "./ScheduleContainer";
+import { renderWithProviders } from "@utils/test-utils";
+
+jest.mock("@fragments/Timetable", () => ({
+  Daily: () => <div>Daily</div>,
+  Weekly: ({ handleDayChange }: { handleDayChange: (day: string) => void }) => (
+    <div onClick={() => handleDayChange("월요일")}>Weekly</div>
+  ),
+}));
+
+describe("<ScheduleContainer />", () => {
+  it("renders", () => {
+    renderWithProviders(<ScheduleContainer />);
+
+    fireEvent.click(screen.getByText("Weekly"));
+  });
+});
