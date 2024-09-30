6d3b9b0f4cf9cb3025d5490cc9e9f0d65060abcf
vietman2 <vietman0@gmail.com>
2024-09-30 21:57:12 +0900

    issue fix

--- baseballhub/src/pages/Schedule/ScheduleContainer.test.tsx
+++ baseballhub/src/pages/Schedule/ScheduleContainer.test.tsx
@@ -5,7 +5,9 @@ import { renderWithProviders } from "@utils/test-utils";
 jest.mock("@fragments/Timetable", () => ({
   Daily: () => <div>Daily</div>,
   Weekly: ({ handleDayChange }: { handleDayChange: (day: string) => void }) => (
-    <div onClick={() => handleDayChange("월요일")}>Weekly</div>
+    <div onClick={() => handleDayChange("월요일")} onKeyDown={jest.fn()}>
+      Weekly
+    </div>
   ),
 }));
 
