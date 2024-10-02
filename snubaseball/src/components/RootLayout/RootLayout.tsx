029d17a0b8ae8f41c291a4ce7915ecb09f19939d
vietman2 <vietman0@gmail.com>
2024-10-03 00:20:47 +0900

    remove iswide logic from rootlayout

--- snubaseball/src/components/RootLayout/RootLayout.tsx
+++ snubaseball/src/components/RootLayout/RootLayout.tsx
@@ -1,33 +1,22 @@
-import { useEffect, useState } from "react";
 import { Outlet } from "react-router-dom";
 import styled from "styled-components";
 
 import { Header } from "@fragments/Header";
-import { useWindowSize } from "@hooks/useWindowSize";
 
 export function RootLayout() {
-  const [isWide, setIsWide] = useState<boolean>(true);
-
-  const { width } = useWindowSize();
-
-  useEffect(() => {
-    setIsWide(width > 1024);
-  }, [width]);
-
   return (
     <>
-      <Header isWide={isWide} />
-      <ContentWrapper $iswide={isWide}>
+      <Header />
+      <ContentWrapper>
         <Outlet />
       </ContentWrapper>
     </>
   );
 }
 
-const ContentWrapper = styled.div<{ $iswide: boolean }>`
+const ContentWrapper = styled.div`
   display: flex;
   flex: 1;
-  padding: ${({ $iswide }) => ($iswide ? "0 20%" : "0 20px")};
 
   overflow-x: hidden;
   box-sizing: border-box;
