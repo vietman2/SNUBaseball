b7cb898f218cafe876797cd95c7d4ad434089ff3
vietman2 <vietman0@gmail.com>
2024-10-02 17:05:33 +0900

    add new icons

--- snubaseball/src/components/Icons/AppIcon.tsx
+++ snubaseball/src/components/Icons/AppIcon.tsx
@@ -1,8 +1,13 @@
+import { ReactComponent as AboutIcon } from "./about.svg";
+import { ReactComponent as ArchiveIcon } from "./archive.svg";
 import { ReactComponent as BaseballIcon } from "./baseball.svg";
 import { ReactComponent as BatIcon } from "./bat.svg";
+import { ReactComponent as CalendarIcon } from "./calendar.svg";
 import { ReactComponent as CheckboxIcon } from "./checkbox.svg";
 import { ReactComponent as GraduateIcon } from "./graduate.svg";
+import { ReactComponent as HomeIcon } from "./home.svg";
 import { ReactComponent as MenuIcon } from "./menu.svg";
+import { ReactComponent as Question } from "./question.svg";
 import { ReactComponent as UserIcon } from "./user.svg";
 
 interface Props {
@@ -13,18 +18,28 @@ interface Props {
 
 export const AppIcon = ({ icon, size, color }: Props) => {
   switch (icon) {
-    case "menu":
-      return <MenuIcon width={size} height={size} color={color} />;
-    case "user":
-      return <UserIcon width={size} height={size} color={color} />;
+    case "about":
+      return <AboutIcon width={size} height={size} color={color} />;
+    case "archive":
+      return <ArchiveIcon width={size} height={size} color={color} />;
     case "baseball":
       return <BaseballIcon width={size} height={size} color={color} />;
     case "bat":
       return <BatIcon width={size} height={size} color={color} />;
+    case "calendar":
+      return <CalendarIcon width={size} height={size} color={color} />;
     case "checkbox":
       return <CheckboxIcon width={size} height={size} color={color} />;
     case "graduate":
       return <GraduateIcon width={size} height={size} color={color} />;
+    case "home":
+      return <HomeIcon width={size} height={size} color={color} />;
+    case "menu":
+      return <MenuIcon width={size} height={size} color={color} />;
+    case "question":
+      return <Question width={size} height={size} color={color} />;
+    case "user":
+      return <UserIcon width={size} height={size} color={color} />;
     default:
       return null;
   }
