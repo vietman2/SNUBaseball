e9e3f42e1b84ef253ccea3e7cca127fc48969a7b
vietman2 <vietman0@gmail.com>
2024-10-08 16:17:34 +0900

    remove unused hook

--- baseballhub/src/hooks/hooks.test.tsx
+++ baseballhub/src/hooks/hooks.test.tsx
@@ -1,60 +1,9 @@
 import { render, screen, waitFor } from "@testing-library/react";
 import "@testing-library/jest-dom";
 
-import { useOrientation } from "./useOrientation";
 import { useWindowSize } from "./useWindowSize";
 
 const TestComponent = () => {
-  const orientation = useOrientation();
-  return <div data-testid="orientation">{orientation}</div>;
-};
-
-describe("useOrientation", () => {
-  const mockMatchMedia = (matches: boolean) => {
-    return jest.fn().mockImplementation(() => ({
-      matches,
-      media: "(orientation: portrait)",
-      addEventListener: jest.fn(),
-      removeEventListener: jest.fn(),
-    }));
-  };
-
-  it("should return 2 for portrait orientation", () => {
-    window.matchMedia = mockMatchMedia(true);
-    render(<TestComponent />);
-    expect(screen.getByTestId("orientation")).toHaveTextContent("2");
-  });
-
-  it("should return 1 for landscape orientation", () => {
-    window.matchMedia = mockMatchMedia(false);
-    render(<TestComponent />);
-    expect(screen.getByTestId("orientation")).toHaveTextContent("1");
-  });
-
-  it("should update orientation when orientation changes", async () => {
-    const addEventListenerMock = jest.fn();
-    const removeEventListenerMock = jest.fn();
-
-    window.matchMedia = jest.fn().mockImplementation(() => ({
-      matches: true, // initial portrait
-      media: "(orientation: portrait)",
-      addEventListener: addEventListenerMock,
-      removeEventListener: removeEventListenerMock,
-    }));
-
-    render(<TestComponent />);
-    expect(screen.getByTestId("orientation")).toHaveTextContent("2");
-
-    await waitFor(() => {
-      window.matchMedia = mockMatchMedia(false);
-      addEventListenerMock.mock.calls[0][1](); // Trigger event listener
-    });
-
-    expect(screen.getByTestId("orientation")).toHaveTextContent("1");
-  });
-});
-
-const TestComponent2 = () => {
   const { width, height } = useWindowSize();
   return (
     <div data-testid="window-size">
@@ -71,12 +20,12 @@ describe("useWindowSize", () => {
   };
 
   it("should return initial window size", () => {
-    render(<TestComponent2 />);
+    render(<TestComponent />);
     expect(screen.getByTestId("window-size")).toHaveTextContent("1024 x 768");
   });
 
   it("should update window size when window resizes", async () => {
-    render(<TestComponent2 />);
+    render(<TestComponent />);
 
     await waitFor(() => {
       resizeWindow(800, 600);
