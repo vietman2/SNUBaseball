a20d9c6ef5ca9eb4f1ae8bffc61b32785e4d2419
vietman2 <vietman0@gmail.com>
2024-10-09 19:04:46 +0900

    small changes to guideline type

--- baseballhub/src/constants/models/guideline.ts
+++ baseballhub/src/constants/models/guideline.ts
@@ -1,3 +1,11 @@
+type TagType = {
+  id: number;
+  name: string;
+  bgColor: string;
+  color: string;
+  icon?: string;
+}
+
 export type GuidelineType = {
   id: number;
   title: string;
@@ -6,7 +14,10 @@ export type GuidelineType = {
   video_id: string;
   description: string;
   equipment: string;
-  tags: string[];
-  likes: number;
+  num_likes: number;
   is_liked: boolean;
+  num_comments: number;
+  is_drill: boolean;
+  is_indoor_possible: boolean;
+  num_people: string;
 };
