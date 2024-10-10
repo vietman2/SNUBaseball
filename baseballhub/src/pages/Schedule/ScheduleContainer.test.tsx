5e1a08d8defdd752a4f67c67717f116dd5a2529c
vietman2 <vietman0@gmail.com>
2024-10-10 20:30:51 +0900

    redo schedule page

--- baseballhub/src/pages/Schedule/ScheduleContainer.test.tsx
+++ baseballhub/src/pages/Schedule/ScheduleContainer.test.tsx
@@ -1,20 +1,30 @@
-import { fireEvent, screen } from "@testing-library/react";
-import ScheduleContainer from "./ScheduleContainer";
-import { renderWithProviders } from "@utils/test-utils";
+import { fireEvent, screen, waitFor } from "@testing-library/react";
 
-jest.mock("@fragments/Timetable", () => ({
-  Daily: () => <div>Daily</div>,
-  Weekly: ({ handleDayChange }: { handleDayChange: (day: string) => void }) => (
-    <div onClick={() => handleDayChange("월요일")} onKeyDown={jest.fn()}>
-      Weekly
+import ScheduleContainer from "./ScheduleContainer";
+import { renderWithProviders, resizeWindow } from "@utils/test-utils";
+
+jest.mock("./WeeklySchedule/WeeklySchedule", () => ({
+  WeeklySchedule: () => <div />,
+}));
+jest.mock("@components/Tabs", () => ({
+  Tabs: ({ setActiveTab }: { setActiveTab: (tab: string) => void }) => (
+    <div>
+      <button onClick={() => setActiveTab("주간 훈참표")}>주간 훈참표</button>
+      <button onClick={() => setActiveTab("월간 캘린더")}>월간 캘린더</button>
+      <button onClick={() => setActiveTab("asdf")}>asdf</button>
     </div>
   ),
 }));
 
 describe("<ScheduleContainer />", () => {
-  it("renders", () => {
+  it("renders", async () => {
     renderWithProviders(<ScheduleContainer />);
 
-    fireEvent.click(screen.getByText("Weekly"));
+    await waitFor(() => resizeWindow(800, 800));
+    await waitFor(() => resizeWindow(600, 600));
+
+    fireEvent.click(screen.getByText("주간 훈참표"));
+    fireEvent.click(screen.getByText("월간 캘린더"));
+    fireEvent.click(screen.getByText("asdf"));
   });
 });
