c6ab9b565bb659c825e93e15df6d683c5436b4dd
vietman2 <vietman0@gmail.com>
2024-10-10 20:31:01 +0900

    remove unused page

--- baseballhub/src/pages/Training/Journals/Journals.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render } from "@testing-library/react";
-
-import Journals from "./Journals";
-
-describe("<Journals />", () => {
-  it("should render", () => {
-    render(<Journals />);
-  });
-});
