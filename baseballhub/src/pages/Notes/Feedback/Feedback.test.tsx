e42ea0153c1ec9ff00171e3a162f917bafb4b260
vietman2 <vietman0@gmail.com>
2024-10-12 01:07:49 +0900

    work feedback page

--- baseballhub/src/pages/Notes/Feedback/Feedback.test.tsx
+++ baseballhub/src/pages/Notes/Feedback/Feedback.test.tsx
@@ -1,6 +1,10 @@
 import { Feedback } from "./Feedback";
 import { renderWithProviders } from "@utils/test-utils";
 
+jest.mock("@fragments/Feedback", () => ({
+  FeedbackList: () => <div>FeedbackList</div>,
+}));
+
 describe("<Feedback />", () => {
   it("renders correctly", () => {
     renderWithProviders(<Feedback />);
