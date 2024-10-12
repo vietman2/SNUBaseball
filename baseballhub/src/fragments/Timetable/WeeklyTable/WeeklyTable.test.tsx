d23272f988797417f4abf02a7b475b4e804db2d1
vietman2 <vietman0@gmail.com>
2024-10-13 02:35:36 +0900

    reduce duplicates

--- baseballhub/src/fragments/Timetable/WeeklyTable/WeeklyTable.test.tsx
+++ baseballhub/src/fragments/Timetable/WeeklyTable/WeeklyTable.test.tsx
@@ -7,18 +7,12 @@ describe("<WeeklyTable />", () => {
   it("renders correctly with reasons and handles header mouse actions", () => {
     renderWithProviders(<WeeklyTable viewReason={true} />);
 
-    fireEvent.mouseOver(screen.getByText("월"));
-    fireEvent.mouseOut(screen.getByText("월"));
-    fireEvent.mouseOver(screen.getByText("화"));
-    fireEvent.mouseOut(screen.getByText("화"));
-    fireEvent.mouseOver(screen.getByText("수"));
-    fireEvent.mouseOut(screen.getByText("수"));
-    fireEvent.mouseOver(screen.getByText("목"));
-    fireEvent.mouseOut(screen.getByText("목"));
-    fireEvent.mouseOver(screen.getByText("금"));
-    fireEvent.mouseOut(screen.getByText("금"));
-    fireEvent.mouseOver(screen.getByText("토"));
-    fireEvent.mouseOut(screen.getByText("토"));
+    const days = ["월", "화", "수", "목", "금", "토"];
+
+    days.forEach((day) => {
+      fireEvent.mouseOver(screen.getByText(day));
+      fireEvent.mouseOut(screen.getByText(day));
+    });
   });
 
   it("renders correctly without reasons", () => {
