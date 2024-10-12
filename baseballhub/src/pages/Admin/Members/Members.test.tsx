847ad54fbb76923a4aec866bc425aaf5e1c39680
vietman2 <vietman0@gmail.com>
2024-10-13 00:12:49 +0900

    reset members

--- baseballhub/src/pages/Admin/Members/Members.test.tsx
+++ baseballhub/src/pages/Admin/Members/Members.test.tsx
@@ -1,22 +1,9 @@
-import { fireEvent, render, screen, waitFor } from "@testing-library/react";
+import { render } from "@testing-library/react";
 
 import { Members } from "./Members";
-import * as MemberAPI from "@services/person/members";
 
 describe("<Members />", () => {
-  it("renders correctly", async () => {
-    jest.spyOn(MemberAPI, "getMembers").mockResolvedValue(false);
-
-    await waitFor(() => render(<Members />));
-
-    fireEvent.click(screen.getByTestId("신입부원 추가"));
-  });
-  
-  it("fetches members list correctly", async () => {
-    jest.spyOn(MemberAPI, "getMembers").mockResolvedValue(true);
-
-    await waitFor(() => render(<Members />));
-
-    fireEvent.click(screen.getByTestId("신입부원 추가"));
+  it("renders correctly", () => {
+    render(<Members />);
   });
 });
--- baseballhub/src/pages/Admin/Members/Members.tsx
+++ baseballhub/src/pages/Admin/Members/Members.tsx
@@ -1,45 +1,10 @@
-import { useEffect, useState } from "react";
 import styled from "styled-components";
 
-import { Chip } from "@components/Chips";
-import { MembersTable } from "@components/Tables";
-import { Tabs } from "@components/Tabs";
-import { PersonType } from "@models/user/person";
-import { getMembers } from "@services/person";
-
-const tabs = ["YB", "OB", "지도자", "기타"];
-
 export function Members() {
-  const [selectedTab, setSelectedTab] = useState<string>("YB");
-  const [members, setMembers] = useState<PersonType[]>([]);
-
-  const fetchMembers = async () => {
-    const data = await getMembers(selectedTab);
-
-    if (data) {
-      setMembers(data);
-    }
-  };
-
-  useEffect(() => {
-    fetchMembers();
-  }, [selectedTab]);
-
   return (
     <>
       <Container>
         <Title>부원관리</Title>
-        <Tabs
-          tabs={tabs}
-          activeTab={selectedTab}
-          setActiveTab={setSelectedTab}
-        />
-        <Content>
-          <Horizontal>
-            <Chip label="신입부원 추가" onClick={() => {}} />
-          </Horizontal>
-          <MembersTable members={members} />
-        </Content>
       </Container>
     </>
   );
@@ -54,19 +19,4 @@ const Container = styled.div`
 
 const Title = styled.h2`
   margin: 1rem 0;
-`;
-
-const Content = styled.div`
-  flex: 1;
-  background-color: white;
-  padding: 1rem 20px;
-  border-bottom-left-radius: 10px;
-  border-bottom-right-radius: 10px;
-`;
-
-const Horizontal = styled.div`
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  margin-bottom: 1rem;
 `;
