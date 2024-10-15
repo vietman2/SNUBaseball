51f475460547f4ba2ef901d2bb995176546447f8
vietman2 <vietman0@gmail.com>
2024-10-15 23:06:32 +0900

    route new api

--- server/core/api_urls.py
+++ server/core/api_urls.py
@@ -1,20 +1,22 @@
 from django.urls import path
+from dj_rest_auth.views import LoginView, LogoutView
 from dj_rest_auth.jwt_auth import get_refresh_view
 from rest_framework.routers import DefaultRouter
-from rest_framework_simplejwt.views import TokenVerifyView
 
 from person.major.views import MajorViewSet
-from person.user.views import RegisterView, StudentIdCheckView
+from person.user.views import RegisterView, StudentIdCheckView, UserProfileView
 
 router = DefaultRouter()
 
 router.register('majors', MajorViewSet, basename='majors')
+router.register('profiles', UserProfileView, basename='profile')
 
 urlpatterns = [
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
     path('register/', RegisterView.as_view(), name='register'),
     path('student_id/', StudentIdCheckView.as_view(), name='student_id_check'),
 
-    path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
     path('token/refresh/', get_refresh_view().as_view(), name='token_refresh'),
 ]
 
