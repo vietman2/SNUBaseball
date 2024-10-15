0b54a375ac3c7d0bb376ee4e11b17aca38cbebed
vietman2 <vietman0@gmail.com>
2024-10-15 16:51:21 +0900

    route new api and modify initial data

--- server/core/api_urls.py
+++ server/core/api_urls.py
@@ -4,12 +4,15 @@ from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenVerifyView
 
 from person.major.views import MajorViewSet
+from person.user.views import RegisterView, StudentIdCheckView
 
 router = DefaultRouter()
 
 router.register('majors', MajorViewSet, basename='majors')
 
 urlpatterns = [
+    path('register/', RegisterView.as_view(), name='register'),
+    path('student_id/', StudentIdCheckView.as_view(), name='student_id_check'),
 
     path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
     path('token/refresh/', get_refresh_view().as_view(), name='token_refresh'),
