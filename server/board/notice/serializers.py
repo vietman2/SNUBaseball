3a1f06a0ce625e5c0fe9fde68d6776b31b73c6e6
vietman2 <vietman0@gmail.com>
2024-10-16 15:35:24 +0900

    create notice app

---
+++ server/board/notice/serializers.py
@@ -0,0 +1,65 @@
+from rest_framework import serializers
+from rest_framework.serializers import ModelSerializer
+
+from person.user.serializers import AuthorSerializer
+from .models import Notice, NoticeCategory, NoticeComment, NoticeContentView
+
+class NoticeCategorySerializer(ModelSerializer):
+    class Meta:
+        model = NoticeCategory
+        fields = ['label', 'color', 'background_color']
+
+class NoticeCommentSerializer(ModelSerializer):
+    author = AuthorSerializer()
+
+    class Meta:
+        model = NoticeComment
+        fields = ['content', 'author', 'created_at']
+
+class NoticeSimpleSerializer(ModelSerializer):
+    category    = NoticeCategorySerializer()
+    author      = serializers.SerializerMethodField()
+    created_at  = serializers.DateTimeField(format="%Y-%m-%d")
+
+    class Meta:
+        model = Notice
+        fields = ['id', 'title', 'category', 'author', 'created_at', 'num_views']
+
+    def get_author(self, obj):
+        return obj.author.member.full_name
+
+class NoticeDetailSerializer(ModelSerializer):
+    category    = NoticeCategorySerializer()
+    author      = AuthorSerializer()
+    comments    = serializers.SerializerMethodField()
+    created_at  = serializers.DateTimeField(format="%Y-%m-%d")
+
+    class Meta:
+        model = Notice
+        fields = [
+            'id', 'title', 'content', 'category', 'author',
+            'created_at', 'num_views', 'comments'
+        ]
+
+    def increment_num_views(self):
+        self.instance.num_views += 1
+        self.instance.save()
+
+        return self.instance
+
+    def content_viewed(self, user):
+        content_view, created = NoticeContentView.objects.get_or_create(
+            user=user,
+            notice=self.instance
+        )
+
+        content_view.viewed_last_at = content_view.viewed_last_at.now()
+        content_view.save()
+
+        return content_view
+
+    def get_comments(self, obj):
+        comments = obj.noticecomment_set.all()
+        serializer = NoticeCommentSerializer(comments, many=True)
+
+        return serializer.data
---
+++ server/board/notice/views.py
@@ -0,0 +1,36 @@
+from drf_spectacular.utils import extend_schema
+from rest_framework import status
+from rest_framework.decorators import action
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet
+
+from .serializers import NoticeSimpleSerializer, NoticeDetailSerializer, NoticeCategorySerializer
+from .models import Notice, NoticeCategory
+
+class NoticeView(ModelViewSet):
+    queryset = Notice.objects.all()
+    serializer_class = NoticeSimpleSerializer
+    permission_classes = [IsAuthenticated,]
+    http_method_names = ['get']
+
+    @extend_schema(summary="공지사항 조회", tags=["공지사항 관리"])
+    def list(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)
+
+    @extend_schema(summary="공지사항 상세 조회", tags=["공지사항 관리"])
+    def retrieve(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = NoticeDetailSerializer(instance)
+
+        serializer.increment_num_views()
+        serializer.content_viewed(request.user)
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    @extend_schema(summary="공지 분류 조회", tags=["공지사항 관리"])
+    @action(detail=False, methods=['get'])
+    def category(self, request, *args, **kwargs):
+        categories = NoticeCategory.objects.all()
+        serializer = NoticeCategorySerializer(categories, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
