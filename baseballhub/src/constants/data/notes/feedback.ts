6dbe7ea756f53208f5f5ec05c03650d15e4eae8e
vietman2 <vietman0@gmail.com>
2024-10-17 00:34:34 +0900

    apply model change

--- baseballhub/src/constants/data/notes/feedback.ts
+++ baseballhub/src/constants/data/notes/feedback.ts
@@ -5,20 +5,20 @@ import {
 } from "@models/notes";
 
 const classification_defense: ClassificationType = {
-  type: "수비",
-  backgroundColor: "#C5CAE9",
+  label: "수비",
+  background_color: "#C5CAE9",
   color: "#1A237E",
 };
 
 const classification_batting: ClassificationType = {
-  type: "타격",
-  backgroundColor: "#FFCCBC",
+  label: "타격",
+  background_color: "#FFCCBC",
   color: "#BF360C",
 };
 
 const classification_running: ClassificationType = {
-  type: "주루",
-  backgroundColor: "#B3E5FC",
+  label: "주루",
+  background_color: "#B3E5FC",
   color: "#01579B",
 };
 
@@ -29,8 +29,8 @@ export const sampleFeedbacks: FeedbackResponseType = {
       title: "타격 시 스윙이 왼팔에 막혀있음",
       content: "왼팔을 좀 더 부드럽게 쓸 것",
       player: "김택원",
-      coach: "이서준",
-      classification: classification_batting,
+      author: "이서준",
+      category: classification_batting,
       status: "완료",
       created_at: "2024-08-01",
       updated_at: "2024-10-01",
@@ -42,8 +42,8 @@ export const sampleFeedbacks: FeedbackResponseType = {
       title: "리드시 시야",
       content: "무조건 투수만 바라보기.",
       player: "이상현",
-      coach: "이정호",
-      classification: classification_running,
+      author: "이정호",
+      category: classification_running,
       status: "진행중",
       created_at: "2024-08-01",
       updated_at: "2024-10-01",
@@ -55,8 +55,8 @@ export const sampleFeedbacks: FeedbackResponseType = {
       title: "외야 타구 송구동작 연결",
       content: "공 빨리 빼고 송구동작으로 연결시킬 것",
       player: "이유용",
-      coach: "이정호",
-      classification: classification_defense,
+      author: "이정호",
+      category: classification_defense,
       status: "검토중",
       created_at: "2024-08-01",
       updated_at: "2024-10-01",
@@ -68,8 +68,8 @@ export const sampleFeedbacks: FeedbackResponseType = {
       title: "스로잉",
       content: "왼발을 끌면서 던지지 말고 바로 내리면서 던질 것",
       player: "심민수",
-      coach: "박건우",
-      classification: classification_batting,
+      author: "박건우",
+      category: classification_batting,
       status: "완료",
       created_at: "2024-08-01",
       updated_at: "2024-10-01",
