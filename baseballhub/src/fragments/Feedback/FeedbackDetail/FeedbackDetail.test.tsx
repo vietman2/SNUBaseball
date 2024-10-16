6e6a2bc3b52eddea0d1cc67b32a9d1ebd7933f26
vietman2 <vietman0@gmail.com>
2024-10-17 01:30:48 +0900

    finish feedback detail and test:

--- baseballhub/src/fragments/Feedback/FeedbackDetail/FeedbackDetail.test.tsx
+++ baseballhub/src/fragments/Feedback/FeedbackDetail/FeedbackDetail.test.tsx
@@ -1,8 +1,31 @@
+import { waitFor } from "@testing-library/react";
+
 import { FeedbackDetail } from "./FeedbackDetail";
+import { sampleFeedbackDetail } from "@data/notes";
+import * as FeedbackAPI from "@services/notes/feedbacks";
 import { renderWithProviders } from "@utils/test-utils";
 
 describe("<FeedbackDetail />", () => {
-  it("renders", () => {
-    renderWithProviders(<FeedbackDetail feedbackId={1} goBack={jest.fn()} />);
+  it("handles null feedbackId correctly", async () => {
+    renderWithProviders(
+      <FeedbackDetail feedbackId={null} goBack={jest.fn()} />
+    );
+  });
+
+  it("handles bad response correctly", async () => {
+    jest.spyOn(FeedbackAPI, "getFeedbackDetail").mockResolvedValue(null);
+    waitFor(() =>
+      renderWithProviders(<FeedbackDetail feedbackId={1} goBack={jest.fn()} />)
+    );
+  });
+
+  it("renders correctly", async () => {
+    jest.spyOn(FeedbackAPI, "getFeedbackDetail").mockResolvedValue({
+      status: 200,
+      data: sampleFeedbackDetail,
+    });
+    await waitFor(() =>
+      renderWithProviders(<FeedbackDetail feedbackId={1} goBack={jest.fn()} />)
+    );
   });
 });
