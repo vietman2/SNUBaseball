60f90f8ed1923795db124bb8a7f1b16ab4b3d93f
vietman2 <vietman0@gmail.com>
2024-10-16 23:11:27 +0900

    implement information detail

--- baseballhub/src/fragments/Information/InformationDetail/InformationDetail.test.tsx
+++ baseballhub/src/fragments/Information/InformationDetail/InformationDetail.test.tsx
@@ -1,10 +1,36 @@
+import { waitFor } from "@testing-library/react";
+
 import { InformationDetail } from "./InformationDetail";
+import { sampleInformationDetail } from "@data/forum";
+import * as InformationAPI from "@services/board/information";
 import { renderWithProviders } from "@utils/test-utils";
 
 describe("<InformationDetail />", () => {
-  it("renders", () => {
+  it("handles null informationId correctly", async () => {
     renderWithProviders(
-      <InformationDetail informationId={1} goBack={jest.fn()} />
+      <InformationDetail informationId={null} goBack={jest.fn()} />
+    );
+  });
+
+  it("handles bad response correctly", async () => {
+    jest.spyOn(InformationAPI, "getInformationDetails").mockResolvedValue(null);
+    waitFor(() =>
+      renderWithProviders(
+        <InformationDetail informationId={1} goBack={jest.fn()} />
+      )
+    );
+  });
+
+  it("renders correctly", async () => {
+    jest.spyOn(InformationAPI, "getInformationDetails").mockResolvedValue({
+      status: 200,
+      data: sampleInformationDetail,
+    });
+
+    await waitFor(() =>
+      renderWithProviders(
+        <InformationDetail informationId={1} goBack={jest.fn()} />
+      )
     );
   });
 });
