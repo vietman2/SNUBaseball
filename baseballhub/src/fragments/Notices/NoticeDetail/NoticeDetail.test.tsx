0ad2511f0e2f6785f2cfe70fd5c5adb9b4f3874c
vietman2 <vietman0@gmail.com>
2024-10-17 23:03:16 +0900

    finish notice detail

--- baseballhub/src/fragments/Notices/NoticeDetail/NoticeDetail.test.tsx
+++ baseballhub/src/fragments/Notices/NoticeDetail/NoticeDetail.test.tsx
@@ -1,9 +1,14 @@
-import { waitFor } from "@testing-library/react";
+import { fireEvent, screen, waitFor } from "@testing-library/react";
 
 import { NoticeDetail } from "./NoticeDetail";
-import { sampleNoticeDetail } from "@data/forum";
+import {
+  sampleNoticeDetail,
+  sampleNoticeDetailWithAttachment,
+} from "@data/forum";
 import * as NoticesAPI from "@services/board/notices";
 import { renderWithProviders } from "@utils/test-utils";
+
+jest.spyOn(window, "open").mockImplementation(() => null);
 
 describe("<NoticeDetail />", () => {
   it("handles null noticeId correctly", async () => {
@@ -24,6 +29,20 @@ describe("<NoticeDetail />", () => {
     });
     await waitFor(() =>
       renderWithProviders(<NoticeDetail noticeId={1} goBack={jest.fn()} />)
+    );
+  });
+
+  it("renders correctly with attachments", async () => {
+    jest.spyOn(NoticesAPI, "getNoticeDetails").mockResolvedValue({
+      status: 200,
+      data: sampleNoticeDetailWithAttachment,
+    });
+    await waitFor(() =>
+      renderWithProviders(<NoticeDetail noticeId={1} goBack={jest.fn()} />)
+    );
+
+    await waitFor(() =>
+      fireEvent.click(screen.getByTestId("유계결석 인정 요청서"))
     );
   });
 });
--- baseballhub/src/fragments/Notices/NoticeDetail/NoticeDetail.tsx
+++ baseballhub/src/fragments/Notices/NoticeDetail/NoticeDetail.tsx
@@ -1,9 +1,11 @@
 import { useEffect, useState } from "react";
 import styled from "styled-components";
+import DOMPurify from "isomorphic-dompurify";
 
 import { Chip } from "@components/Chips";
 import { Divider } from "@components/Dividers";
 import { ErrorComponent, Loading } from "@components/Fallbacks";
+import { AppIcon } from "@components/Icons";
 import { Subtitle } from "@components/Texts";
 import { NoticeDetailType } from "@models/forum";
 import { getNoticeDetails } from "@services/board";
@@ -17,6 +19,10 @@ export function NoticeDetail({ noticeId, goBack }: Readonly<Props>) {
   const [notice, setNotice] = useState<NoticeDetailType>();
   const [loading, setLoading] = useState<boolean>(false);
   const [error, setError] = useState<boolean>(false);
+
+  const handleDownload = (attachment: string) => {
+    window.open(attachment);
+  };
 
   useEffect(() => {
     const fetchNoticeDetails = async () => {
@@ -69,13 +75,36 @@ export function NoticeDetail({ noticeId, goBack }: Readonly<Props>) {
       <DividerWrapper>
         <Divider bold />
       </DividerWrapper>
+      <AttachmentsWrapper>
+        <Subtitle>첨부파일 ({notice.attachments.length})</Subtitle>
+        {notice.attachments.map((attachment) => (
+          <button
+            onClick={() => handleDownload(attachment.file)}
+            key={attachment.created_at}
+            data-testid={`${attachment.name}`}
+          >
+            <Attachment>
+              <div>{attachment.name}</div>
+              <AppIcon icon="download" size={24} color="gray" />
+            </Attachment>
+          </button>
+        ))}
+      </AttachmentsWrapper>
+      <DividerWrapper>
+        <Divider bold />
+      </DividerWrapper>
       <Content>
-        <div>{notice.content}</div>
+        <div
+          dangerouslySetInnerHTML={{
+            __html: DOMPurify.sanitize(notice.content),
+          }}
+        />
       </Content>
       <DividerWrapper>
         <Divider bold />
       </DividerWrapper>
       <Comments>
+        <Subtitle>댓글</Subtitle>
         {notice.comments.map((comment, index) => (
           <div key={index}>{comment}</div>
         ))}
@@ -100,6 +129,24 @@ const Header = styled.div`
   font-weight: 700;
 `;
 
+const AttachmentsWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+  padding: 8px 16px;
+`;
+
+const Attachment = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+
+  &:hover {
+    cursor: pointer;
+  }
+`;
+
 const ChipWrapper = styled.div`
   display: flex;
   padding: 8px 12px;
@@ -120,7 +167,7 @@ const Content = styled.div`
   flex: 1;
   flex-direction: column;
   min-height: 300px;
-  padding: 16px 8px;
+  padding: 8px;
 
   color: ${({ theme }) => theme.colors.foreground500};
 `;
