704396d46f4f2b4b750af298d04ce511797dfbcf
정승원 <vietman2@jeongseung-won-ui-Macmini.local>
2024-12-19 04:10:17 +0900

    initial config

---
+++ server/snuserver/settings/local.py
@@ -0,0 +1,55 @@
+from .base import * # pylint: disable=W0401,W0614
+
+SECRET_KEY = config("SECRET_KEY")
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = ['localhost', '192.168.45.248']
+
+# Database
+# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+REST_AUTH = {
+    'USE_JWT': True,
+    'JWT_AUTH_COOKIE': 'access',
+    'JWT_AUTH_REFRESH_COOKIE': 'refresh',
+    'JWT_AUTH_SECURE': False,
+    'JWT_AUTH_HTTPONLY': True,
+    'JWT_AUTH_RETURN_EXPIRATION': True,
+    'JWT_AUTH_COOKIE_USE_CSRF' : True,
+    'SESSION_LOGIN': False,
+
+    'USER_DETAILS_SERIALIZER': 'person.user.serializers.ProfileSerializer',
+}
+
+AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY')
+AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_KEY')
+AWS_STORAGE_BUCKET_NAME = 'snubaseball.test'
+AWS_S3_ENDPOINT_URL = 'https://kr.object.ncloudstorage.com'
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
+}
+
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+MEDIA_URL = 'https://kr.object.ncloudstorage.com/snubaseball.test/'
+FALLBACK_IMAGE = 'https://kr.object.ncloudstorage.com/snubaseball.test/profiles/person.png'
+
+NAVER_CLIENT_ID = config("NAVER_API_KEY_ID")
+NAVER_CLIENT_SECRET = config("NAVER_API_KEY")
+
+# CORS
+CORS_ALLOWED_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+]
--- server/snuserver/urls.py
+++ server/snuserver/urls.py
@@ -14,9 +14,23 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularSwaggerView as SSV,
+    SpectacularRedocView
+)
 
 urlpatterns = [
+    path('v1/', include('core.api_urls')),
     path('admin/', admin.site.urls),
+
+    path('schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('schema/swagger-ui/', SSV.as_view(url_name='schema'), name='swagger-ui'),
+    path('schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc')
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
