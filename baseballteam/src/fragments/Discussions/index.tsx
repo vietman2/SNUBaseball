a7024e2efd581a126b8ca7adcee4150b4db4c5e5
정승원 <vietman2@jeongseung-won-ui-Macmini.local>
2024-12-22 03:50:07 +0900

    add discussion fragments

---
+++ baseballteam/src/fragments/Discussions/index.tsx
@@ -0,0 +1,7 @@
+import { DiscussionCard } from "./DiscussionsCard/DiscussionsCard";
+import {
+  DiscussionsTableHeader,
+  DiscussionsTableRow,
+} from "./DiscussionsTable/DiscussionsTableRow";
+
+export { DiscussionCard, DiscussionsTableHeader, DiscussionsTableRow };
