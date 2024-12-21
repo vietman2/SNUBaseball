3ba4c7604715c8fdfc20b22bfd7bf05f4180bb68
정승원 <vietman2@jeongseung-won-ui-Macmini.local>
2024-12-22 00:48:03 +0900

    pylint

--- server/core/api_urls.py
+++ server/core/api_urls.py
@@ -26,29 +26,28 @@ router.register(
 )
 router.register('notices', NoticeView, basename='notices')
 
-"""
-router.register(
-    'discussions/(?P<discussion_id>[0-9]+)/comments',
-    DiscussionCommentView,
-    basename='discussion_comments'
-)
-router.register('discussions', DiscussionView, basename='discussions')
+#router.register(
+#    'discussions/(?P<discussion_id>[0-9]+)/comments',
+#    DiscussionCommentView,
+#    basename='discussion_comments'
+#)
+#router.register('discussions', DiscussionView, basename='discussions')
 
-router.register('equipment', EquipmentCategoryView, basename='equipment_categories')
+#router.register('equipment', EquipmentCategoryView, basename='equipment_categories')
 
-router.register(
-    'feedbacks/(?P<feedback_id>[0-9]+)/comments',
-    FeedbackCommentView,
-    basename='feedback_comments'
-)
-router.register('feedbacks', FeedbackView, basename='feedbacks')
-router.register(
-    'guidelines/(?P<guideline_id>[0-9]+)/comments',
-    GuidelineCommentView,
-    basename='guideline_comments'
-)
-router.register('guidelines', GuidelineView, basename='guidelines')
-"""
+#router.register(
+#    'feedbacks/(?P<feedback_id>[0-9]+)/comments',
+#    FeedbackCommentView,
+#    basename='feedback_comments'
+#)
+#router.register('feedbacks', FeedbackView, basename='feedbacks')
+#router.register(
+#    'guidelines/(?P<guideline_id>[0-9]+)/comments',
+#    GuidelineCommentView,
+#    basename='guideline_comments'
+#)
+#router.register('guidelines', GuidelineView, basename='guidelines')
+
 router.register('majors', MajorViewSet, basename='majors')
 router.register('members', MemberViewSet, basename='members')
 router.register('profiles', UserProfileView, basename='profile')
