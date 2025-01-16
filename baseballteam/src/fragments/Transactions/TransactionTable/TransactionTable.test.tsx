837053b381e99ff5e3d9ae07cca09d45682c77f3
정승원 <vietman2@jeongseung-won-ui-Macmini.local>
2025-01-16 12:14:11 +0900

    styles

--- baseballteam/src/fragments/Transactions/TransactionTable/TransactionTable.test.tsx
+++ baseballteam/src/fragments/Transactions/TransactionTable/TransactionTable.test.tsx
@@ -1,9 +1,11 @@
+import { waitFor } from "@testing-library/react";
+
 import {
   TransactionTableHeader,
   TransactionTableRow,
 } from "./TransactionTable";
 import { sampleTransactions } from "@data/accountings";
-import { renderWithProviders } from "@utils/test-utils";
+import { renderWithProviders, resizeWindow } from "@utils/test-utils";
 
 describe("<TransactionTableHeader />", () => {
   it("should render", () => {
@@ -12,12 +14,15 @@ describe("<TransactionTableHeader />", () => {
 });
 
 describe("<TransactionTableRow />", () => {
-  it("should render", () => {
+  it("should render", async () => {
     renderWithProviders(
       <>
         <TransactionTableRow transaction={sampleTransactions[0]} />
         <TransactionTableRow transaction={sampleTransactions[1]} />
       </>
     );
+
+    await waitFor(() => resizeWindow(400, 400));
+    await waitFor(() => resizeWindow(1600, 1600));
   });
 });
