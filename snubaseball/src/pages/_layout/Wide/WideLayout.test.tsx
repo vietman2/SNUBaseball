88d88aa73e19ec61afd3d8c541f628a5d881f65c
정승원 <vietman2@jeongseung-won-ui-MacBookPro.local>
2025-02-03 20:24:45 +0900

    minor

--- snubaseball/src/pages/_layout/Wide/WideLayout.test.tsx
+++ snubaseball/src/pages/_layout/Wide/WideLayout.test.tsx
@@ -1,6 +1,7 @@
+import { fireEvent, screen } from "@testing-library/react";
+
 import { WideLayout } from "./WideLayout";
 import * as NavigationContext from "@contexts/navigation";
-import { fireEvent, screen } from "@testing-library/dom";
 import { renderWithProviders } from "@utils/test-utils";
 
 describe("<WideLayout />", () => {
