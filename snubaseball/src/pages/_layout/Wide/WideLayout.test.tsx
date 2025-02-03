d25b4ec9dbdafd682decf1f98d4eb322313f7020
정승원 <vietman2@jeongseung-won-ui-MacBookPro.local>
2025-02-03 20:21:23 +0900

    work wide layout

--- snubaseball/src/pages/_layout/Wide/WideLayout.test.tsx
+++ snubaseball/src/pages/_layout/Wide/WideLayout.test.tsx
@@ -1,8 +1,27 @@
 import { WideLayout } from "./WideLayout";
+import * as NavigationContext from "@contexts/navigation";
+import { fireEvent, screen } from "@testing-library/dom";
 import { renderWithProviders } from "@utils/test-utils";
 
 describe("<WideLayout />", () => {
-  it("should render", () => {
+  it("should render and handle navigation", () => {
+    const { tabs } = jest.requireActual("@contexts/navigation");
+
+    jest.spyOn(NavigationContext, "useNavigation").mockReturnValue({
+      currentTab: tabs[0],
+      tabs,
+      setCurrentTab: jest.fn(),
+      setCurrentSubTab: jest.fn(),
+    });
+
     renderWithProviders(<WideLayout />);
+
+    fireEvent.click(screen.getByTestId("home-button"));
+    fireEvent.click(screen.getByTestId("tab-아카이브"));
+    fireEvent.click(screen.getByTestId("tab-소개"));
+    fireEvent.click(screen.getByTestId("tab-연혁"));
+
+    fireEvent.mouseOver(screen.getByTestId("tabs"));
+    fireEvent.mouseOut(screen.getByTestId("wide-layout"));
   });
 });
--- snubaseball/src/pages/_layout/Wide/WideLayout.tsx
+++ snubaseball/src/pages/_layout/Wide/WideLayout.tsx
@@ -1,11 +1,139 @@
+import { useState } from "react";
+import { Outlet, useNavigate } from "react-router-dom";
 import styled from "styled-components";
 
+import { Logo } from "@components/Icons";
+import { TabType, useNavigation } from "@contexts/navigation";
+
 export function WideLayout() {
-  return <Container>Wide</Container>;
+  const [menuOpen, setMenuOpen] = useState<boolean>(false);
+
+  const { tabs } = useNavigation();
+  const navigate = useNavigate();
+
+  const handleHomeClick = () => {
+    navigate("/");
+  };
+
+  const handleTabClick = (tab: TabType) => {
+    if (tab.subtabs.length > 0) {
+      return;
+    }
+
+    navigate(tab.path);
+  };
+
+  return (
+    <Container>
+      <div onMouseLeave={() => setMenuOpen(false)} data-testid="wide-layout">
+        <Header>
+          <button onClick={handleHomeClick} data-testid="home-button">
+            <Logo size={48} />
+          </button>
+          <Tabs onMouseOver={() => setMenuOpen(true)} data-testid="tabs">
+            {tabs.map((tab) => (
+              <div key={tab.title}>
+                {tab.path !== "/" && (
+                  <button
+                    onClick={() => handleTabClick(tab)}
+                    data-testid={`tab-${tab.title}`}
+                  >
+                    {tab.title}
+                  </button>
+                )}
+              </div>
+            ))}
+          </Tabs>
+        </Header>
+        <Menu $isOpen={menuOpen}>
+          {tabs.map((tab) => (
+            <Tabs key={tab.title}>
+              {tab.subtabs.length > 0 ? (
+                <>
+                  {tab.subtabs.map((subtab) => (
+                    <button
+                      key={subtab.title}
+                      onClick={() => navigate(subtab.path)}
+                      data-testid={`tab-${subtab.title}`}
+                    >
+                      {subtab.title}
+                    </button>
+                  ))}
+                </>
+              ) : (
+                <div />
+              )}
+            </Tabs>
+          ))}
+        </Menu>
+      </div>
+      <Contents>
+        <Outlet />
+      </Contents>
+    </Container>
+  );
 }
 
 const Container = styled.div`
   position: relative;
   width: 100%;
   height: 100dvh;
+  background-color: ${({ theme }) => theme.colors.background100};
+`;
+
+const Header = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+  height: 80px;
+  padding: 0 15%;
+
+  background-color: ${({ theme }) => theme.colors.background500};
+
+  > button:first-child {
+    display: flex;
+    align-items: center;
+    padding: 8px 16px;
+  }
+`;
+
+const Tabs = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 36px;
+
+  > button {
+    width: 64px;
+  }
+
+  > div {
+    width: 64px;
+  }
+`;
+
+const Menu = styled.div<{ $isOpen: boolean }>`
+  display: flex;
+  max-height: ${({ $isOpen }) => ($isOpen ? "200px" : "0")};
+  width: 100%;
+  justify-content: flex-end;
+  padding: 0 15%;
+  gap: 36px;
+
+  background-color: ${({ theme }) => theme.colors.background300};
+  transition: max-height 0.3s ease-in-out;
+  overflow: hidden;
+
+  > div {
+    flex-direction: column;
+    padding: 16px 0;
+    gap: 24px;
+  }
+`;
+
+const Contents = styled.div`
+  display: flex;
+  flex: 1;
 `;
