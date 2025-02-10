c114365b766353681709780d2e4647fd001f032b
정승원 <vietman2@jeongseung-won-ui-MacBookPro.local>
2025-02-10 17:22:30 +0900

    add test player data

---
+++ snubaseball/src/data/about.ts
@@ -0,0 +1,64 @@
+import { PlayerType } from "@models/about";
+
+export const samplePlayers: PlayerType[] = [
+  {
+    id: "1",
+    name: "선수 1",
+    backNumber: 1,
+    profileImage: "https://picsum.photos/200/300?random=1",
+  },
+  {
+    id: "2",
+    name: "선수 2",
+    backNumber: 2,
+    profileImage: "https://picsum.photos/200/300?random=2",
+  },
+  {
+    id: "3",
+    name: "선수 3",
+    backNumber: 3,
+    profileImage: "https://picsum.photos/200/300?random=3",
+  },
+  {
+    id: "4",
+    name: "선수 4",
+    backNumber: 4,
+    profileImage: "https://picsum.photos/200/300?random=4",
+  },
+  {
+    id: "5",
+    name: "선수 5",
+    backNumber: 5,
+    profileImage: "https://picsum.photos/200/300?random=5",
+  },
+  {
+    id: "6",
+    name: "선수 6",
+    backNumber: 6,
+    profileImage: "https://picsum.photos/200/300?random=6",
+  },
+  {
+    id: "7",
+    name: "선수 7",
+    backNumber: 7,
+    profileImage: "https://picsum.photos/200/300?random=7",
+  },
+  {
+    id: "8",
+    name: "선수 8",
+    backNumber: 8,
+    profileImage: "https://picsum.photos/200/300?random=8",
+  },
+  {
+    id: "9",
+    name: "선수 9",
+    backNumber: 9,
+    profileImage: "https://picsum.photos/200/300?random=9",
+  },
+  {
+    id: "10",
+    name: "선수 10",
+    backNumber: 10,
+    profileImage: "https://picsum.photos/200/300?random=10",
+  },
+];
---
+++ snubaseball/src/models/about.ts
@@ -0,0 +1,6 @@
+export type PlayerType = {
+  id: string;
+  name: string;
+  backNumber: number;
+  profileImage: string;
+};
