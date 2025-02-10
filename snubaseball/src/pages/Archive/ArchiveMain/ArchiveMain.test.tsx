81332993ec35d05727ebd1b8b2049318b90ed5c4
정승원 <vietman2@jeongseung-won-ui-MacBookPro.local>
2025-02-10 17:11:45 +0900

    finish archive main

--- snubaseball/src/pages/Archive/ArchiveMain/ArchiveMain.test.tsx
+++ snubaseball/src/pages/Archive/ArchiveMain/ArchiveMain.test.tsx
@@ -1,8 +1,20 @@
+import { fireEvent, screen } from "@testing-library/react";
+
 import { ArchiveMain } from "./ArchiveMain";
 import { renderWithProviders } from "@utils/test-utils";
+
+jest.mock("@fragments/Interviews", () => ({
+  Interview: () => <div>Interview</div>,
+}));
+jest.mock("@fragments/Memories", () => ({
+  Memories: () => <div>Memories</div>,
+}));
 
 describe("<ArchiveMain />", () => {
   it("renders correctly", () => {
     renderWithProviders(<ArchiveMain />);
+
+    fireEvent.click(screen.getByTestId("more-memories"));
+    fireEvent.click(screen.getByTestId("more-interviews"));
   });
 });
--- snubaseball/src/pages/Archive/ArchiveMain/ArchiveMain.tsx
+++ snubaseball/src/pages/Archive/ArchiveMain/ArchiveMain.tsx
@@ -1,8 +1,38 @@
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 
 import { Divider } from "@components/Dividers";
+import { sampleInterviews, sampleMemories } from "@data/archives";
+import { Interview } from "@fragments/Interviews";
+import { Memories } from "@fragments/Memories";
+import { InterviewType, MemoriesType } from "@models/archive";
 
 export function ArchiveMain() {
+  const [memories, setMemories] = useState<MemoriesType[]>([]);
+  const [interviews, setInterviews] = useState<InterviewType[]>([]);
+
+  const navigate = useNavigate();
+
+  const navigateToMemories = () => {
+    navigate("/archive/memories");
+  };
+
+  const navigateToInterviews = () => {
+    navigate("/archive/interviews");
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      // TODO: Fetch memories data from the server
+
+      setMemories(sampleMemories);
+      setInterviews(sampleInterviews);
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <Container>
       <Wrapper>
@@ -10,12 +40,27 @@ export function ArchiveMain() {
           MEMORIES
           <span>_순간의 기록</span>
         </Subtitle>
-        <Divider text="2024" type="dashed" />
+        {memories.map((memory) => (
+          <Memories key={memory.year} memories={memory} />
+        ))}
       </Wrapper>
-      <Subtitle>
-        INTERVIEW
-        <span>_우리들의 이야기</span>
-      </Subtitle>
+      <Divider />
+      <Button onClick={navigateToMemories} data-testid="more-memories">
+        {"MORE >>"}
+      </Button>
+      <Wrapper>
+        <Subtitle>
+          INTERVIEW
+          <span>_우리들의 이야기</span>
+        </Subtitle>
+        {interviews.map((interview) => (
+          <Interview key={interview.id} interview={interview} />
+        ))}
+      </Wrapper>
+      <Divider />
+      <Button onClick={navigateToInterviews} data-testid="more-interviews">
+        {"MORE >>"}
+      </Button>
     </Container>
   );
 }
@@ -24,17 +69,20 @@ const Container = styled.div`
   display: flex;
   flex: 1;
   flex-direction: column;
-  padding: 16px 24px;
-  gap: 24px;
+  margin: 16px 0;
+  padding: 16px 0;
+  gap: 8px;
 `;
 
 const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
+  padding: 16px 0;
   gap: 16px;
 `;
 
 const Subtitle = styled.span`
+  padding: 0 16px;
   font-size: 1.5rem;
   font-weight: 700;
 
@@ -42,4 +90,12 @@ const Subtitle = styled.span`
     font-size: 1rem;
     font-weight: 400;
   }
+`;
+
+const Button = styled.button`
+  display: flex;
+  justify-content: flex-start;
+  padding: 0 24px;
+
+  color: ${({ theme }) => theme.colors.primary};
 `;
