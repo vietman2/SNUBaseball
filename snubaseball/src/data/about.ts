0776146a95d3ed4a3abff824b8d83d1c17736a55
정승원 <vietman2@jeongseung-won-ui-MacBookPro.local>
2025-03-17 02:50:28 +0900

    add and modify data type

--- snubaseball/src/data/about.ts
+++ snubaseball/src/data/about.ts
@@ -1,65 +1,35 @@
-import { HistoryType, PlayerType } from "@models/about";
+import { HistoryType, PlayerType, ManagerType } from "@models/about";
+
+export const sampleManagers: ManagerType[] = [
+  {
+    id: "1",
+    name: "매니저 1",
+    profile_image: "https://picsum.photos/200/300?random=1",
+    major: "컴퓨터공학과",
+    admission_year: 2021,
+    role: "매니저",
+    back_number: 0,
+  },
+];
 
 export const samplePlayers: PlayerType[] = [
   {
     id: "1",
     name: "선수 1",
-    backNumber: 1,
-    profileImage: "https://picsum.photos/200/300?random=1",
+    back_number: 1,
+    profile_image: "https://picsum.photos/200/300?random=1",
+    position: "투수",
+    major: "컴퓨터공학과",
+    admission_year: 2021,
   },
   {
     id: "2",
     name: "선수 2",
-    backNumber: 2,
-    profileImage: "https://picsum.photos/200/300?random=2",
-  },
-  {
-    id: "3",
-    name: "선수 3",
-    backNumber: 3,
-    profileImage: "https://picsum.photos/200/300?random=3",
-  },
-  {
-    id: "4",
-    name: "선수 4",
-    backNumber: 4,
-    profileImage: "https://picsum.photos/200/300?random=4",
-  },
-  {
-    id: "5",
-    name: "선수 5",
-    backNumber: 5,
-    profileImage: "https://picsum.photos/200/300?random=5",
-  },
-  {
-    id: "6",
-    name: "선수 6",
-    backNumber: 6,
-    profileImage: "https://picsum.photos/200/300?random=6",
-  },
-  {
-    id: "7",
-    name: "선수 7",
-    backNumber: 7,
-    profileImage: "https://picsum.photos/200/300?random=7",
-  },
-  {
-    id: "8",
-    name: "선수 8",
-    backNumber: 8,
-    profileImage: "https://picsum.photos/200/300?random=8",
-  },
-  {
-    id: "9",
-    name: "선수 9",
-    backNumber: 9,
-    profileImage: "https://picsum.photos/200/300?random=9",
-  },
-  {
-    id: "10",
-    name: "선수 10",
-    backNumber: 10,
-    profileImage: "https://picsum.photos/200/300?random=10",
+    back_number: 2,
+    profile_image: "https://picsum.photos/200/300?random=2",
+    position: "내야수",
+    major: "컴퓨터공학과",
+    admission_year: 2021,
   },
 ];
 
