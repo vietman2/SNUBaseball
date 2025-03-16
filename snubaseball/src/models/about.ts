d30966dc5ed7796de3c470c14665212763ee6074
정승원 <vietman2@jeongseung-won-ui-MacBookPro.local>
2025-03-17 01:13:02 +0900

    add history data types and test data

--- snubaseball/src/models/about.ts
+++ snubaseball/src/models/about.ts
@@ -4,3 +4,15 @@ export type PlayerType = {
   backNumber: number;
   profileImage: string;
 };
+
+export type HistoryType = {
+  year: number;
+  professor: string;
+  head_coach: string;
+  head_manager: string;
+  captain: string;
+  vice_captain: string;
+  games: number;
+  num_players: number;
+  num_managers: number;
+}
