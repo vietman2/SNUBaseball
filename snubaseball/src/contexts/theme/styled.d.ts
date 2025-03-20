8738f31c7267e26198412dadea2295d39183154a
정승원 <vietman2@jeongseung-won-ui-MacBookPro.local>
2025-03-20 13:05:37 +0900

    add colors

--- snubaseball/src/contexts/theme/styled.d.ts
+++ snubaseball/src/contexts/theme/styled.d.ts
@@ -5,6 +5,7 @@ export type ThemeColors = {
   secondary: string;
   tertiary: string;
   background100: string;
+  background200: string;
   background300: string;
   background500: string;
   background700: string;
