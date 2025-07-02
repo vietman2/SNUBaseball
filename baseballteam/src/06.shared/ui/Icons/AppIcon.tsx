44003b5fd0ae6e7065b12d9020a6caca45f91e71
정승원 <vietman2@jeongseung-won-ui-MacBookPro.local>
2025-07-02 09:56:31 +0900

    add new icons

--- baseballteam/src/06.shared/ui/Icons/AppIcon.tsx
+++ baseballteam/src/06.shared/ui/Icons/AppIcon.tsx
@@ -4,10 +4,12 @@ import ChevronDown from "./files/chevron-down.svg?react";
 import ChevronLeft from "./files/chevron-left.svg?react";
 import ChevronRight from "./files/chevron-right.svg?react";
 import ChevronUp from "./files/chevron-up.svg?react";
+import Close from "./files/close.svg?react";
 import Diary from "./files/diary.svg?react";
 import Forum from "./files/forum.svg?react";
 import Home from "./files/home.svg?react";
 import Management from "./files/management.svg?react";
+import Menu from "./files/menu.svg?react";
 import Money from "./files/money.svg?react";
 import Moon from "./files/moon.svg?react";
 import Person from "./files/person.svg?react";
@@ -30,10 +32,12 @@ const iconMap: Record<
   "chevron-left": ChevronLeft,
   "chevron-right": ChevronRight,
   "chevron-up": ChevronUp,
+  close: Close,
   diary: Diary,
   forum: Forum,
   home: Home,
   management: Management,
+  menu: Menu,
   money: Money,
   moon: Moon,
   person: Person,
