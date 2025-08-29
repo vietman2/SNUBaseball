c15b7821c45e80b3dfb44846c53f233243532442
정승원 <vietman2@jeongseung-won-ui-MacBookPro.local>
2025-08-29 14:15:10 +0900

    migrate

--- baseballserver/member/major/apps.py
+++ baseballserver/member/major/apps.py
@@ -3,5 +3,5 @@ from django.apps import AppConfig
 
 class MajorConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "auth.major"
+    name = "member.major"
     label = "major"
