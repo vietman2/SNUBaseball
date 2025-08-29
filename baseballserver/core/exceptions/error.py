981824ed009bebd34881eca1f2b8bed609af3f24
정승원 <vietman2@jeongseung-won-ui-MacBookPro.local>
2025-08-29 16:02:03 +0900

    redefine exceptions

---
+++ baseballserver/core/exceptions/error.py
@@ -0,0 +1,35 @@
+from rest_framework.exceptions import APIException
+
+
+class SNUBaseballException(APIException):
+    status = "ERROR"
+    code = "UNKNOWN_ERROR"
+    default_detail = "서버에서 알 수 없는 오류가 발생했습니다."
+    default_status_code = 400
+
+    def __init__(self, detail=None, status=None, code=None, status_code=None):
+        self.detail = detail or self.default_detail
+        self.status = status or self.status
+        self.code = code or self.code
+        self.status_code = status_code or self.default_status_code
+
+
+class UnauthorizedException(SNUBaseballException):
+    status = "UNAUTHORIZED"
+    code = "UNAUTHORIZED"
+    default_detail = "인증되지 않은 사용자입니다."
+    default_status_code = 401
+
+
+class ForbiddenException(SNUBaseballException):
+    status = "FORBIDDEN"
+    code = "FORBIDDEN"
+    default_detail = "권한이 없는 사용자입니다."
+    default_status_code = 403
+
+
+class NotFoundException(SNUBaseballException):
+    status = "NOT_FOUND"
+    code = "NOT_FOUND"
+    default_detail = "요청한 리소스를 찾을 수 없습니다."
+    default_status_code = 404
---
+++ baseballserver/core/exceptions/handler.py
@@ -0,0 +1,66 @@
+from django.http import Http404
+from rest_framework.exceptions import (
+    AuthenticationFailed,
+    NotAuthenticated,
+    PermissionDenied,
+    ValidationError,
+)
+from rest_framework.views import exception_handler
+
+from .error import SNUBaseballException
+
+
+def baseball_server_exception_handler(exc, context):
+    """
+    커스텀 예외 처리 함수
+    """
+    response = exception_handler(exc, context)
+
+    if isinstance(exc, SNUBaseballException):
+        response.data = {
+            "status": exc.status,
+            "code": exc.code,
+            "message": exc.detail,
+        }
+
+        return response
+
+    if isinstance(exc, (NotAuthenticated, AuthenticationFailed, PermissionDenied)):
+        response.data = {
+            "status": "UNAUTHORIZED",
+            "code": "UNAUTHORIZED",
+            "message": str(exc.detail) or "인증되지 않은 사용자입니다.",
+        }
+
+        return response
+
+    if isinstance(exc, Http404):
+        response.data = {
+            "status": "NOT_FOUND",
+            "code": "NOT_FOUND",
+            "message": "요청한 리소스를 찾을 수 없습니다.",
+        }
+
+        return response
+
+    if isinstance(exc, ValidationError):
+        response.data = {
+            "status": "ERROR",
+            "code": "VALIDATION_ERROR",
+            "message": exc.detail,
+        }
+
+        return response
+
+    if response is not None:
+        response.data = {
+            "status": "ERROR",
+            "code": "UNKNOWN_ERROR",
+            "message": response.data.get(
+                "detail", "서버에서 알 수 없는 오류가 발생했습니다."
+            ),
+        }
+
+        return response
+
+    return response
