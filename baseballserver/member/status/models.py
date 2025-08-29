0ec82608245e71eff3f789bb74acf5700a6ceeb0
정승원 <vietman2@jeongseung-won-ui-MacBookPro.local>
2025-08-29 14:18:51 +0900

    add the new apps

---
+++ baseballserver/member/status/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+from core.models import Chip
+
+
+class MemberStatus(Chip):
+    name = models.CharField(max_length=20, unique=True)
+
+    objects = models.Manager()
+
+    class Meta:
+        db_table = "member_status"
+        verbose_name = "부원 활동 상태"
+        verbose_name_plural = "부원 활동 상태"
+
+    def __str__(self):
+        return self.name
