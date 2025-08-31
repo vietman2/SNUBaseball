a39d2efd8ed481abf70a117171d622e74142d036
정승원 <vietman2@jeongseung-won-ui-MacBookPro.local>
2025-09-01 03:35:26 +0900

    update user model

--- baseballserver/auth/user/models.py
+++ baseballserver/auth/user/models.py
@@ -2,7 +2,7 @@ import uuid
 from django.contrib.auth.models import AbstractBaseUser
 from django.db import models
 
-from auth.member.models import Member
+from member.person.models import Member
 from .managers import UserManager
 
 
@@ -14,19 +14,21 @@ class User(AbstractBaseUser):
     joined_at = models.DateTimeField(auto_now_add=True)
 
     is_superuser = models.BooleanField(default=False)
-    is_staff = models.BooleanField(default=False)
-    is_blocked = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
 
     USERNAME_FIELD = "username"
 
     objects = UserManager()
 
-    def has_perm(self, perm, obj=None):  # pylint: disable=unused-argument
-        return True
+    @property
+    def is_staff(self):
+        return self.is_superuser
 
-    def has_module_perms(self, app_label):  # pylint: disable=unused-argument
-        return True
+    def has_perm(self, perm, obj=None):
+        return self.is_superuser
+
+    def has_module_perms(self, app_label):
+        return self.is_superuser
 
     class Meta:
         db_table = "user"
--- baseballserver/auth/user/serializers/__init__.py
+++
@@ -1,4 +0,0 @@
-from .profile_serializer import ProfileSerializer
-from .register_serializer import RegisterSerializer
-
-__all__ = ["ProfileSerializer", "RegisterSerializer"]
