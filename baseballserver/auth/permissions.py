21c43c474238d365382b14f06b5228315f9077d4
정승원 <vietman2@jeongseung-won-ui-MacBookPro.local>
2025-09-01 03:35:43 +0900

    add permissions

---
+++ baseballserver/auth/permissions.py
@@ -0,0 +1,16 @@
+from rest_framework.permissions import BasePermission
+
+
+class IsSelf(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return obj == request.user
+
+
+class IsAuthenticated(BasePermission):
+    def has_permission(self, request, view):
+        return request.user and request.user.is_authenticated and request.user.is_active
+
+
+class IsAdmin(IsAuthenticated):
+    def has_permission(self, request, view):
+        return super().has_permission(request, view) and request.user.is_superuser
