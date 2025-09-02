41153187fe0a9557ef8cb0946f3d13522a141954
정승원 <vietman2@jeongseung-won-ui-MacBookPro.local>
2025-09-03 04:04:27 +0900

    add logout feature

--- baseballteam/src/01.app/router/layouts/profile/__tests__/MyProfileModal.test.tsx
+++ baseballteam/src/01.app/router/layouts/profile/__tests__/MyProfileModal.test.tsx
@@ -1,10 +1,20 @@
-import { describe, it } from "vitest";
+import { describe, it, vi } from "vitest";
+import { fireEvent } from "@testing-library/react";
 
 import { MyProfileModal } from "../MyProfileModal";
 import { renderWithProviders } from "@test-utils/renderer";
 
 describe("MyProfileModal", () => {
-  it("renders without crashing", () => {
-    renderWithProviders(<MyProfileModal />);
+  it("handles logout", () => {
+    vi.spyOn(window, "alert").mockImplementation(() => {});
+    const { getByText } = renderWithProviders(<MyProfileModal />);
+   
+    // cancel logout first time
+    vi.spyOn(window, "confirm").mockReturnValueOnce(false);
+    fireEvent.click(getByText("로그아웃"));
+
+    // confirm logout second time
+    vi.spyOn(window, "confirm").mockReturnValueOnce(true);
+    fireEvent.click(getByText("로그아웃"));
   });
 });
---
+++ baseballteam/src/04.features/auth/logout/ui/LogoutButton.tsx
@@ -0,0 +1,51 @@
+import styled from "styled-components";
+
+import { logout } from "../api/logout";
+import { useColors } from "@shared/lib/styles";
+import { AppIcon } from "@shared/ui/Icons";
+import { useTokens } from "@shared/lib/auth";
+
+export function LogoutButton() {
+  const { colors } = useColors();
+  const { clearToken } = useTokens();
+
+  const handleLogout = async () => {
+    if (window.confirm("Are you sure you want to log out?")) {
+      await logout();
+
+      clearToken();
+      window.alert("Successfully logged out.");
+    }
+  };
+
+  return (
+    <Button onClick={handleLogout} data-testid="logout-button">
+      <span>
+        <AppIcon icon="logout" size={16} color={colors.error} />
+      </span>
+      <span>로그아웃</span>
+    </Button>
+  );
+}
+
+const Button = styled.button`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  padding: 8px 12px;
+  gap: 12px;
+  width: 100%;
+
+  font-size: 14px;
+  font-weight: 600;
+  color: ${({ theme }) => theme.colors.error};
+
+  background-color: ${({ theme }) => theme.colors.gray400};
+  border-radius: 8px;
+
+  > span:first-child {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+`;
