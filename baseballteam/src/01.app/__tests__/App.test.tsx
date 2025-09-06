d5ed929afe611c4fd97d66e001a4edca60c8b93d
정승원 <vietman2@jeongseung-won-ui-MacBookPro.local>
2025-09-07 02:13:32 +0900

    Add test

---
+++ baseballteam/src/01.app/__tests__/App.test.tsx
@@ -0,0 +1,11 @@
+import { describe, expect, it } from "vitest";
+import { render } from "@testing-library/react";
+
+import App from "../App";
+
+describe("App", () => {
+  it("renders without crashing", () => {
+    const { container } = render(<App />);
+    expect(container).toBeInTheDocument();
+  });
+});
