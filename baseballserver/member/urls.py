655c5c846fa8b53531775f8e19349ab55de8baf4
정승원 <vietman2@jeongseung-won-ui-MacBookPro.local>
2025-09-07 00:24:38 +0900

    route new api

--- baseballserver/member/urls.py
+++ baseballserver/member/urls.py
@@ -1,9 +1,12 @@
 from rest_framework.routers import DefaultRouter
 
+from .major.views import MajorViewSet
 from .person.views import ProfileViewSet
 
 router = DefaultRouter()
 
+router.register(r"v1/majors", MajorViewSet, basename="majors")
 router.register(r"v1/profiles", ProfileViewSet, basename="profiles")
+
 
 urlpatterns = router.urls
