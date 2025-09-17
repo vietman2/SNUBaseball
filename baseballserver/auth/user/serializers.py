f0a050e303406e78bbca916d4f9099d8bfee5136
정승원 <vietman2@jeongseung-won-ui-MacBookPro.local>
2025-09-17 14:06:25 +0900

    add role as a field to return

--- baseballserver/auth/user/serializers.py
+++ baseballserver/auth/user/serializers.py
@@ -6,12 +6,23 @@ from .models import User
 
 class UserProfileSerializer(serializers.ModelSerializer):
     username = serializers.SerializerMethodField()
+    role = serializers.SerializerMethodField()
     member = ProfileSerializer()
 
     class Meta:
         model = User
-        fields = ["uuid", "username", "member"]
+        fields = ["uuid", "username", "role", "member"]
 
     def get_username(self, obj):
         ## 마스킹 처리
         return obj.username[:3] + "****"
+
+    def get_role(self, obj):
+        if obj.is_superuser:
+            return "ADMIN"
+        elif obj.member and obj.member.role.id in [1, 2, 3]:
+            return "LEADER"
+        elif obj.member and obj.member.role.id in [6, 7, 8]:
+            return "STAFF"
+        else:
+            return "MEMBER"
