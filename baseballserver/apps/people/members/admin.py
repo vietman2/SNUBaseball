d0d7ef72dda62104e22d7cbb3298ffe1a0c5d247
정승원 <vietman2@jeongseung-won-ui-MacBookPro.local>
2025-09-24 01:42:10 +0900

    refactor admin files to fix bug

--- baseballserver/apps/people/members/admin.py
+++ baseballserver/apps/people/members/admin.py
@@ -1,12 +1,9 @@
 from django.contrib import admin
 
-from .members.models import College, Department, Member, MemberRole, MemberStatus
-from .users.models import User
+from .models import College, Department, Member, MemberRole, MemberStatus
 
 admin.site.register(College)
 admin.site.register(Department)
 admin.site.register(Member)
 admin.site.register(MemberRole)
 admin.site.register(MemberStatus)
-
-admin.site.register(User)
