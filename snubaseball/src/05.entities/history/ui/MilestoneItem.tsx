be6324df3fd205e4b9b5a8f4aa2f53acb28005fc
정승원 <vietman2@jeongseung-won-ui-MacBookPro.local>
2025-09-23 17:55:53 +0900

    temporarily remove unused to pass cicd

--- snubaseball/src/05.entities/history/ui/MilestoneItem.tsx
+++ snubaseball/src/05.entities/history/ui/MilestoneItem.tsx
@@ -1,6 +1,4 @@
-import Image from "next/image";
-
-import { Event, MilestoneContainer, MilestoneImage, Year } from "./styles";
+import { Event, MilestoneContainer, Year } from "./styles";
 import { MilestoneType } from "../models/history";
 import { ImagePlaceholder } from "@shared/ui/Images";
 
@@ -19,18 +17,7 @@ export function MilestoneItem({ milestone }: Readonly<Props>) {
         </Event>
       ))}
       {milestone.imageUrl && (
-        <>
           <ImagePlaceholder />
-          {/*}
-        <MilestoneImage>
-          <Image
-            src={milestone.imageUrl}
-            alt={milestone.year}
-            width={280}
-            height={280}
-          />
-        </MilestoneImage>*/}
-        </>
       )}
     </MilestoneContainer>
   );
