6c6f7aad4b9d837c45686ca9be5df718f75f9200
정승원 <vietman2@jeongseung-won-ui-MacBookPro.local>
2025-10-01 16:11:36 +0900

    customize response

--- baseballserver/apps/media/gallery/paginators.py
+++ baseballserver/apps/media/gallery/paginators.py
@@ -1,4 +1,5 @@
 from rest_framework.pagination import PageNumberPagination
+from rest_framework.response import Response
 
 
 class MediaPageNumberPagination(PageNumberPagination):
@@ -6,3 +7,16 @@ class MediaPageNumberPagination(PageNumberPagination):
     page_query_param = "page"
     page_size_query_param = "page_size"
     max_page_size = 100
+
+    def get_paginated_response(self, data):
+        page_size = self.get_page_size(self.request) or self.page.paginator.per_page
+
+        return Response(
+            {
+                "count": self.page.paginator.count,
+                "pages": self.page.paginator.num_pages,
+                "page": self.page.number,
+                "page_size": page_size,
+                "results": data,
+            }
+        )
