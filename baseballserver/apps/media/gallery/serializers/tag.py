7e82d950435f794669eecf7795da2f0cc4bd37b7
정승원 <vietman2@jeongseung-won-ui-MacBookPro.local>
2025-10-08 02:31:04 +0900

    add num images and num videos to tags serializer

--- baseballserver/apps/media/gallery/serializers/tag.py
+++ baseballserver/apps/media/gallery/serializers/tag.py
@@ -4,7 +4,16 @@ from ..models import MediaTag
 
 
 class MediaTagSerializer(serializers.ModelSerializer):
+    num_images = serializers.SerializerMethodField()
+    num_videos = serializers.SerializerMethodField()
+
     class Meta:
         model = MediaTag
-        fields = ["id", "name"]
+        fields = ["id", "name", "color", "icon", "num_images", "num_videos"]
         read_only_fields = ["id"]
+
+    def get_num_images(self, obj):
+        return obj.images.count()
+
+    def get_num_videos(self, obj):
+        return obj.videos.count()
