17226360faea4f4df91078ff6b394383dc8bd283
정승원 <vietman2@jeongseung-won-ui-MacBookPro.local>
2025-10-07 22:39:39 +0900

    bugfix

--- baseballteam/src/04.features/gallery/media/upload/ui/SelectMediaMeta.tsx
+++ baseballteam/src/04.features/gallery/media/upload/ui/SelectMediaMeta.tsx
@@ -1,12 +1,14 @@
 import styled from "styled-components";
 
 import { useUploadMediaForm } from "../contexts/useUploadMediaForm";
-import { AlbumSelect } from "@entities/gallery/album";
-import { TagsSelect } from "@entities/gallery/tags";
+import { AlbumSelect, useAlbums } from "@entities/gallery/album";
+import { TagsSelect, useTags } from "@entities/gallery/tags";
 import { useColors } from "@shared/lib/styles";
 import { AppIcon } from "@shared/ui/Icons";
 
 export function SelectMediaMeta() {
+  const { albums } = useAlbums();
+  const { tags } = useTags();
   const { selectedAlbum, selectAlbum, selectedTags, selectTag } =
     useUploadMediaForm();
   const { colors } = useColors();
@@ -17,13 +19,21 @@ export function SelectMediaMeta() {
         <SelectLabel>
           <AppIcon icon="album" color={colors.primary} size={16} /> 앨범 선택
         </SelectLabel>
-        <AlbumSelect selectedAlbum={selectedAlbum} selectAlbum={selectAlbum} />
+        <AlbumSelect
+          albums={albums}
+          selectedAlbum={selectedAlbum}
+          selectAlbum={selectAlbum}
+        />
       </SelectWrapper>
       <SelectWrapper>
         <SelectLabel>
           <AppIcon icon="tag" color={colors.primary} size={16} /> 태그 선택
         </SelectLabel>
-        <TagsSelect selectedTags={selectedTags} selectTag={selectTag} />
+        <TagsSelect
+          tags={tags}
+          selectedTags={selectedTags}
+          selectTag={selectTag}
+        />
       </SelectWrapper>
     </Header>
   );
