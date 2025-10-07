92d94dfa78deb1127c36d27023570f0d1583d355
정승원 <vietman2@jeongseung-won-ui-MacBookPro.local>
2025-10-08 02:30:48 +0900

    simplify album serializer

--- baseballserver/apps/media/gallery/serializers/album.py
+++ baseballserver/apps/media/gallery/serializers/album.py
@@ -1,11 +1,10 @@
 from rest_framework import serializers
 
-from .media import GalleryImageSerializer
 from ..models import Album
 
 
 class AlbumSerializer(serializers.ModelSerializer):
-    cover_image = serializers.SerializerMethodField()
+    cover_image_url = serializers.SerializerMethodField()
     num_images = serializers.IntegerField(read_only=True, source="images.count")
     num_videos = serializers.IntegerField(read_only=True, source="videos.count")
 
@@ -15,15 +14,23 @@ class AlbumSerializer(serializers.ModelSerializer):
             "id",
             "title",
             "members_only",
-            "cover_image",
+            "color",
+            "cover_image_url",
             "num_images",
             "num_videos",
         ]
 
-    def get_cover_image(self, obj):
+    def get_cover_image_url(self, obj):
         ## random image from the album.
         images = obj.images.all()
         if images.exists():
             image = images.order_by("?").first()
-            return GalleryImageSerializer(image).data
+            return image.image.url
         return None
+
+
+class AlbumSimpleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Album
+        fields = ["id", "title", "members_only", "color"]
+        read_only_fields = ["id"]
